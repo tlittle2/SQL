6471bb4ff0bc375f207b126b53e4ad0cf36b2faa
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2024-12-25 18:03:49 -0500

    Create simonsays.sql

---
+++ Oracle/PL_SQL/Kattis/simonsays.sql
@@ -0,0 +1,12 @@
+DECLARE
+  type v_arr is table of varchar(100);
+	arr v_arr := v_arr('Raise your right hand.','Lower your right hand.','Simon says raise your left hand.');
+	saying char(10) := 'Simon says';
+
+BEGIN
+    for i in 1..arr.COUNT loop
+    	if instr(arr(i), saying) > 0 then
+    		dbms_output.put_line(substr(arr(i), instr(arr(i) , saying) + length(saying), length(arr(i))));
+    	end if;
+    end loop;
+END;
