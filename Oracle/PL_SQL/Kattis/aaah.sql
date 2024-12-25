45b0f9fe1d1856200f04a1ba051e1d4478afb60b
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2024-12-25 14:32:32 -0500

    Create aaah.sql

---
+++ Oracle/PL_SQL/Kattis/aaah.sql
@@ -0,0 +1,12 @@
+DECLARE
+	ip1 varchar2(999) := 'ahhhhhh';
+	ip2 varchar2(999) := 'aahh';
+	
+BEGIN
+	if length(ip1) >= length(ip2) then
+    	dbms_output.put_line('go');
+    else
+        dbms_output.put_line('no');
+    end if;
+
+END;
