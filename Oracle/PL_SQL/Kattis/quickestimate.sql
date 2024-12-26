1241ac501a78b1f54b43bf1441066ad2b285cda6
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2024-12-25 19:19:32 -0500

    Create quickestimate.sql

---
+++ Oracle/PL_SQL/Kattis/quickestimate.sql
@@ -0,0 +1,16 @@
+DECLARE
+  type v_arr is table of integer;
+	ip_arr v_arr := v_arr();
+
+	cases integer := 6;
+			
+BEGIN
+    for i in 1..cases loop
+    	ip_arr.EXTEND;
+		  ip_arr(i):= dbms_random.value(1,10000);
+    end loop;
+
+	for i in 1..ip_arr.COUNT LOOP
+        dbms_output.put_line(ip_arr(i) || '-> ' || length(ip_arr(i)));
+    END LOOP;
+END;
