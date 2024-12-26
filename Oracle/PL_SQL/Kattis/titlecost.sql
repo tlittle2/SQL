34673d675e523b102b75578ab684b80ec2a6b73c
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2024-12-25 19:12:33 -0500

    Create titlecost.sql

---
+++ Oracle/PL_SQL/Kattis/titlecost.sql
@@ -0,0 +1,16 @@
+DECLARE
+	ip varchar2(100) := 'PieHard 3.14159265358979323846';
+
+	ip_title VARCHAR2(100) := SUBSTR(ip, 1, INSTR(ip, ' ') - 1);
+  ip_cost FLOAT(10) := CAST(TRIM(SUBSTR(ip, INSTR(ip, ' ') + 1)) AS FLOAT);
+
+  title_length INTEGER := LENGTH(ip_title);
+		
+BEGIN
+    
+	IF ip_cost < title_length THEN
+        DBMS_OUTPUT.PUT_LINE(ip_cost);
+    ELSE
+        DBMS_OUTPUT.PUT_LINE(title_length);
+    END IF;
+END;
