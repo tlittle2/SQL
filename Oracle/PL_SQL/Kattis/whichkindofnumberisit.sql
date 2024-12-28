0dc255c8d0b5624a59e662c7c5a4caf63c28bcf1
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2024-12-28 14:30:55 -0500

    Create whichkindofnumberisit.sql

---
+++ Oracle/PL_SQL/Kattis/whichkindofnumberisit.sql
@@ -0,0 +1,46 @@
+DECLARE
+  type t_arr is table of integer;
+	numbers t_arr := t_arr(1,7,20,9);
+	sqrt_value number;
+	ans varchar(2);
+
+	FUNCTION isSquare(n INTEGER) RETURN BOOLEAN IS
+    sqrt_value number:= sqrt(n);
+    BEGIN
+        if trunc(sqrt_value) = sqrt_value then
+        return True;
+        end if;
+    	return False;
+    END;
+
+	FUNCTION isOdd(n INTEGER) RETURN BOOLEAN IS
+    BEGIN
+        if mod(n,2) = 1 then
+        return True;
+        end if;
+    	return False;
+    END;
+
+	PROCEDURE displayAnswer(str VARCHAR2) is
+    BEGIN
+        if length(str) > 0 then
+        dbms_output.put_line(ans);
+		else
+        dbms_output.put_line('EMPTY');
+        end if;
+		ans:= '';
+	END;
+
+BEGIN
+    for i in numbers.FIRST..numbers.LAST loop
+		if isOdd(numbers(i)) then
+            ans:= ans || 'O';
+		end if;
+		
+		if isSquare(numbers(i)) then
+            ans:= ans || 'S';
+        end if;
+		
+		displayAnswer(ans);
+    end loop;
+END;
