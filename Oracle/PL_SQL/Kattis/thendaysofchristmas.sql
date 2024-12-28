accc783b4501746d71a6fea8f81f06aa51a27bcc
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2024-12-28 15:58:14 -0500

    Update thendaysofchristmas.sql

--- Oracle/PL_SQL/Kattis/thendaysofchristmas.sql
+++ Oracle/PL_SQL/Kattis/thendaysofchristmas.sql
@@ -1,37 +1,37 @@
-DECLARE
+DECLARE    
 	type t_arr is table of integer index by pls_integer;
 	arr t_arr := t_arr();
-	ip integer:= 12;
-
-	out1 integer:= 0;
-	out2 integer:= 0;
+	ip integer:= 12; --user input
 
 	PROCEDURE processInput is
     	begin
     		for i in 1..ip loop
         		arr(i):= i;
         	end loop;
-    	end;
+    end;
 
-	PROCEDURE calculateOutput1 is
+	FUNCTION calculateOutput1(a IN t_arr) return INTEGER IS
+    	out1 integer:= 0;
     	begin
         	for i in arr.first..arr.last loop
 			out1:= out1 +arr(i);
 		end loop;
-    	end;
+		
+		return out1;
+    end;
 
-	PROCEDURE calculateOutput2 is
+	FUNCTION calculateOutput2(a IN t_arr) return INTEGER IS
+    	out2 integer:= 0;
     	begin
         	for i in 1..ip loop
 	        	for j in 1..i loop
     				out2:= out2 + j;
         		end loop;
     		end loop;
+		return out2;
     	end;
     
 BEGIN
 	processInput;
-	calculateOutput1;
-    	calculateOutput2;	
-	dbms_output.put_line(out1 || ' ' || out2);
+	dbms_output.put_line(calculateOutput1(arr) || ' ' || calculateOutput2(arr));
 END;
