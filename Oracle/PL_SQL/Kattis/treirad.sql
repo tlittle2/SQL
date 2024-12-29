cb5ed9fedff2776a1d442cea219dfa39dbe1730f
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2024-12-28 20:50:54 -0500

    Create treirad.sql

---
+++ Oracle/PL_SQL/Kattis/treirad.sql
@@ -0,0 +1,16 @@
+DECLARE
+    ip integer:= 1234;
+
+	strt integer:= 1;
+	cnt integer:=0;		
+
+BEGIN
+    loop
+      exit when strt * (strt + 1) * (strt + 2) >= ip;
+      cnt:= cnt+1;
+  	  strt:= strt+1;
+	  end loop;
+
+	dbms_output.put_line(cnt);
+	
+END;
