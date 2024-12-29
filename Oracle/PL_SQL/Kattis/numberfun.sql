031d638f8f1023d08bf4d94a63fea1d71811f4bb
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2024-12-28 21:50:41 -0500

    Create numberfun.sql

---
+++ Oracle/PL_SQL/Kattis/numberfun.sql
@@ -0,0 +1,18 @@
+DECLARE
+  type t_arr is table of integer;
+	nums t_arr := t_arr(5,3,1);
+
+BEGIN
+    if nums(1) + nums(2) = nums(3) then
+    	dbms_output.put_line('Possible');
+    elsif nums(1) - nums(2) = nums(3) or nums(2) - nums(1) = nums(3) then
+    	dbms_output.put_line('Possible');
+    elsif nums(1) * nums(2) = nums(3) then
+    	dbms_output.put_line('Possible');
+    elsif nums(1) / nums(2) = nums(3) or nums(2) / nums(1) = nums(3) then
+    	dbms_output.put_line('Possible');
+    else
+    	dbms_output.put_line('Impossible');
+    end if;
+
+END;
