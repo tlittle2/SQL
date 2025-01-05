3ade2cd5af71bd4ad62f9de24dcfd77cb9dbf7bb
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-01-05 12:03:57 -0500

    Update tripletexting2.sql

--- Oracle/PL_SQL/Kattis/Triple Texting/tripletexting2.sql
+++ Oracle/PL_SQL/Kattis/Triple Texting/tripletexting2.sql
@@ -1,49 +1,49 @@
 DECLARE
-	subtype ipLength is varchar2(99);
-	
-	type t_arr is table of ipLength index by pls_integer;
-	arr t_arr := t_arr();
+    subtype ipLength is varchar2(99);
+    type t_arr is table of ipLength index by pls_integer;
 
 	type c_arr is table of pls_integer index by ipLength;
 	occur c_arr := c_arr();
 
-	function processInput(a IN OUT t_arr) return t_arr is
-	        const_split CONSTANT INTEGER := 3;
-	        ip ipLength := 'trevortrevertrevor';
-	    	len integer := 1;
-	        maxlength integer := floor(length(ip)/const_split);
-    	begin
-        	for i in 1..const_split loop
-    			a(i) := substr(ip, len, maxlength);
-			len := len + maxlength;
-    		end loop;
-	return a;
-    	end;
+	function processInput return t_arr is
+        a t_arr := t_arr();
+        const_split CONSTANT INTEGER := 3;
+        ip ipLength := 'trevortrevertrevor';
+    	len integer := 1;
+        maxlength integer := floor(length(ip)/const_split);
+    begin
+        for i in 1..const_split loop
+    	a(i) := substr(ip, len, maxlength);
+		len := len + maxlength;
+    	end loop;
+		return a;
+    end;
 	
 
 	procedure createOccurrenceArray(a IN t_arr, occurArray IN OUT c_arr) IS
-    	BEGIN
-        	for i in a.FIRST..a.LAST loop
-        		if not occurArray.EXISTS(arr(i)) then --if current word from processInput array does not exists in occurrence array, add it
-        		occurArray(arr(i)) := 1;	
-        		else
-        		occurArray(arr(i)) := occurArray(arr(i)) + 1;
-			end if;
-    		end loop;
-    	END;
+    BEGIN
+        for i in a.FIRST..a.LAST loop
+        if not occurArray.EXISTS(a(i)) then --if current word from processInput array does not exists in occurrence array, add it
+        	occurArray(a(i)) := 1;	
+        else
+        	occurArray(a(i)) := occurArray(a(i)) + 1;
+		end if;
+    	end loop;
+    END;
 
 	procedure findAnswer(occurArray IN c_arr) is
     	tmpKey ipLength := occur.FIRST;
-    	begin
-    		while tmpKey is not null loop
-            	if occurArray(tmpKey) > 1 then
-            		dbms_output.put_line(tmpKey);
-    			exit;
-            	end if;
-    			tmpKey := occurArray.NEXT(tmpKey);
-        	end loop;
-    	end;
+    begin
+    	while tmpKey is not null loop
+            if occurArray(tmpKey) > 1 then
+            dbms_output.put_line(tmpKey);
+    		exit;
+            end if;
+    		tmpKey := occurArray.NEXT(tmpKey);
+        end loop;
+    end;
+
 BEGIN
-	createOccurrenceArray(processInput(arr), occur);
+	createOccurrenceArray(processInput, occur);
 	findAnswer(occur);
 END;
