22c04785298737185bf52dd14518b2e11e6ce711
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-01-10 20:41:28 -0500

    Update alphabetspam2.sql

--- Oracle/PL_SQL/Kattis/AlphabetSpam/alphabetspam2.sql
+++ Oracle/PL_SQL/Kattis/AlphabetSpam/alphabetspam2.sql
@@ -1,5 +1,5 @@
 DECLARE
-    ipString varchar2(32767) := '\/\/in_US$100000_in_our_Ca$h_Lo||ery!!!';
+   	ipString varchar2(32767) := '\/\/in_US$100000_in_our_Ca$h_Lo||ery!!!';
 
 	type t_chars is table of char(1);
 
@@ -15,53 +15,49 @@ DECLARE
 
 	
 	procedure populateStaticCollections(p_letters IN OUT t_chars, p_isUpper IN BOOLEAN DEFAULT TRUE) is 
-    begin
-        if p_isUpper then
-            for i in ascii('A')..ascii('Z') loop
-        	p_letters.EXTEND;
-    		p_letters(i - ascii('A') + 1):= chr(i);
-        	end loop;
-        else
-            for i in ascii('a')..ascii('z') loop
-        	p_letters.EXTEND;
-    		p_letters(i - ascii('a') + 1):= chr(i);
-        	end loop;
-
-    	end if;
+    	begin
+	        if p_isUpper then
+	            for i in ascii('A')..ascii('Z') loop
+	        	p_letters.EXTEND;
+	    		p_letters(i - ascii('A') + 1):= chr(i);
+	        	end loop;
+	        else
+	            for i in ascii('a')..ascii('z') loop
+	        	p_letters.EXTEND;
+	    		p_letters(i - ascii('a') + 1):= chr(i);
+	        	end loop;
+	
+	    	end if;
         
-    end;
+	end;
 
 	procedure populateIPTable(p_ipString IN VARCHAR2, p_ipLetters IN OUT t_chars) is
-    begin
-        for i in 1..length(p_ipString) loop
-        p_ipLetters.EXTEND;
-		p_ipLetters(i) := substr(p_ipString, i, 1);
-    	end loop;
+    	begin
+        	for i in 1..length(p_ipString) loop
+	        	p_ipLetters.EXTEND;
+			p_ipLetters(i) := substr(p_ipString, i, 1);
+    		end loop;
 
-    end;
+    	end;
 
 
 	function isMember(p_chr IN CHAR, p_collection IN t_chars) return BOOLEAN is
-    begin
-        if p_chr member of p_collection then
-        return true;
+    	begin
+        	if p_chr member of p_collection then
+        	return true;
 		end if;
-
 		return false;
-    end;
+    	end;
 
 
 	procedure printAnswer(p_ipNumber IN PLS_INTEGER, p_ipString VARCHAR2 DEFAULT ipString) is
-    begin
+    	begin
         dbms_output.put_line(round(p_ipNumber / length(ipString),15));
-
-
-    end;
-	
+    	end;
 	
 BEGIN
 
-    populateStaticCollections(upperLetters, TRUE);
+    	populateStaticCollections(upperLetters, TRUE);
 	populateStaticCollections(lowerLetters,FALSE);
 	populateIPTable(ipString,ipLetters);
 	
@@ -73,7 +69,7 @@ BEGIN
 			when isMember(ipLetters(i), t_chars('_')) then whiteSpacesNumber:= whiteSpacesNumber + 1;
 			else symbols:= symbols + 1;
 		end case;
-    end loop;
+    	end loop;
 
 	printAnswer(whiteSpacesNumber);
 	printAnswer(lowerNumber);
