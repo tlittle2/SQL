e93f24045d51987e7d0e8f27591ab7277ded3375
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-01-12 15:01:48 -0500

    Update karte.sql
    Normalized some types into subtypes. First sample input is still not correct

--- Oracle/PL_SQL/Kattis/Karte/karte.sql
+++ Oracle/PL_SQL/Kattis/Karte/karte.sql
@@ -1,32 +1,34 @@
-DECLARE    
+DECLARE
     type t_cardNumber is table of number;
 
-    type t_arr is table of t_cardNumber index by varchar2(1);
+	subtype st_keylength is varchar2(1);
+    type t_arr is table of t_cardNumber index by st_keylength;
 	cards t_arr;
 
-	type t_ansArr is table of varchar2(6);
+	subtype st_ansLength is varchar2(6);
+	type t_ansArr is table of st_ansLength;
 	ansArr t_ansArr := t_ansArr();
+	
+	greska constant st_ansLength := 'GRESKA';
 
-	type t_cardsList is table of char(1);
+	type t_cardsList is table of st_keylength;
 	possibleCards t_cardsList := t_cardsList('P', 'K', 'H', 'T');
 
-	ip varchar2(1000) := 'P01K02H03H04';
-
-	greska constant varchar2(6) := 'GRESKA';
+	ip varchar2(1000) := 'H02H10P11H02'; --user input
 
 	procedure processInput(p_ipString IN VARCHAR2, p_collection IN OUT t_arr) is
-        v_idxIdx number := 1;
+        v_Idx number := 1;
     	const_sublength constant number :=3;
     	v_word varchar2(3);
-    	v_key varchar2(1);
+    	v_key st_keylength;
     	v_value varchar2(2);
     begin
         loop
-        	if v_idxIdx > length(p_ipString) then
+        	if v_Idx > length(p_ipString) then
         		exit;
     		end if;
 
-    		v_word := substr(p_ipString, v_idxIdx, const_sublength);
+    		v_word := substr(p_ipString, v_Idx, const_sublength);
     		v_key := substr(v_word, 1,1);
     		v_value := to_number(substr(v_word, 2,2));
     
@@ -38,15 +40,15 @@ DECLARE
 
 			p_collection(v_key)(p_collection(v_key).COUNT) := v_value; --insert new key/value pair to the end of the list
     				
-    		v_idxIdx:= v_idxIdx +const_sublength;
+    		v_Idx:= v_Idx +const_sublength;
 		end loop;
     end;
 
 	procedure checkOtherCards(p_collection IN OUT t_arr) is 
-		v_idx varchar2(1):= p_collection.FIRST;
+		v_idx st_keylength:= p_collection.FIRST;
     begin
         while v_idx is not null loop
-        if v_idx not member of possibleCards then
+        if v_idx not member of possibleCards then --if this card is not in the list of possible cards, add new collection with a sole value of 0
         	p_collection(v_idx) := t_cardNumber(0);
         end if;
 			v_idx := p_collection.NEXT(v_idx);
@@ -55,7 +57,7 @@ DECLARE
 
 
 	procedure processCollection(p_collection IN t_arr, p_ans IN OUT t_ansArr) is
-    v_idx varchar2(1) := cards.FIRST;
+    v_idx st_keylength := p_collection.FIRST;
 	distinctCards t_cardNumber;
 	
 	mx CONSTANT number := 13;
@@ -63,9 +65,8 @@ DECLARE
     begin
         while v_idx is not null loop
         	p_ans.EXTEND;
-        	distinctCards := t_cardNumber();
-            distinctCards := distinctCards multiset union distinct p_collection(v_idx);
-			
+            distinctCards := t_cardNumber() multiset union distinct p_collection(v_idx); -- get the distinct values out of the nested table
+
 			if distinctCards.COUNT < p_collection(v_idx).COUNT then
                 p_ans(p_ans.COUNT) := greska;
 
@@ -79,8 +80,8 @@ DECLARE
     	end loop;
     end;
 
-	procedure printCollection(p_collection in t_arr) is
-        v_idx varchar2(1) := p_collection.FIRST;
+	procedure printInputCollection(p_collection in t_arr) is
+        v_idx st_keylength := p_collection.FIRST;
     begin
         while v_idx is not null loop
 		for i in p_collection(v_idx).FIRST..p_collection(v_idx).LAST loop
@@ -90,18 +91,22 @@ DECLARE
     end loop;
     end;
 
+	procedure printAns(p_answerCollection in t_ansArr) is
+    begin
+        if greska member of p_answerCollection then
+        dbms_output.put_line(greska);
+        else	
+        	for i in p_answerCollection.FIRST..p_answerCollection.LAST LOOP
+                dbms_output.put_line(p_answerCollection(i));
+            end loop;
+    	end if;
+    end;
+
 BEGIN
     processInput(ip, cards);
 	checkOtherCards(cards);
-	printCollection(cards);
+	--printInputCollection(cards);
 	processCollection(cards,ansArr);
-
-	if greska member of ansArr then
-        dbms_output.put_line(greska);
-    else	
-    	for i in ansArr.FIRST..ansArr.LAST LOOP
-            dbms_output.put_line(ansArr(i));
-        end loop;
-	end if;
+	printAns(ansArr);
 
 END;
