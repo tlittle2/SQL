f9033caf7c3657ef04843bfcd2b10ca3545852be
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-01-12 11:53:16 -0500

    Create knightpacking.sql

---
+++ Oracle/PL_SQL/Kattis/knightpacking.sql
@@ -0,0 +1,16 @@
+DECLARE    
+	ip number := 1;
+
+	function answer(p_ip number) return VARCHAR2 is
+    begin
+        if mod(p_ip, 2) = 1 then
+    	return 'first';
+    	else
+		return 'second';
+    	end if;
+    end;
+
+BEGIN
+    dbms_output.put_line(answer(ip));
+	
+END;
