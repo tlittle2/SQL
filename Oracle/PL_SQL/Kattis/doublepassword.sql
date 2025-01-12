7aa69822431b2bb2ee02ba07e84117fa1f01e533
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-01-12 11:30:42 -0500

    Update doublepassword.sql

--- Oracle/PL_SQL/Kattis/doublepassword.sql
+++ Oracle/PL_SQL/Kattis/doublepassword.sql
@@ -1,30 +1,36 @@
 DECLARE
 	type arr_t is table of varchar2(4);
-	arr1 arr_t:= arr_t();
-	arr2 arr_t:= arr_t();
+	arr1 arr_t;
+	arr2 arr_t;
     
-	ip1 varchar(4) := '1111';
-	ip2 varchar(4) := '1234';
+	ip1 varchar2(4) := '1111';
+	ip2 varchar2(4) := '1234';
 
-	c integer:= 0;
+	procedure populateCollection(p_ipString IN VARCHAR2, p_collection IN OUT arr_t) is
+    	begin
+	        p_collection := arr_t();
+	        for i in 1..length(p_ipString)
+	        loop
+			p_collection.EXTEND;
+			p_collection(i):= substr(p_ipString, i, 1);
+	        end loop;
+    	end;
+
+	function answer(p_collection1 IN arr_t, p_collection2 IN arr_t) return INTEGER IS
+        c integer:= 0;
+	begin
+		for i in 1..p_collection1.COUNT
+		loop
+		if p_collection1(i) <> p_collection2(i) then
+			c:= c + 1;
+		end if;
+    		end loop;
+	return power(2,c);
+    	end;
 
 BEGIN
-    for i in 1..length(ip1) loop
-    	arr1.EXTEND;
-		arr1(i):= substr(ip1, i, 1);
-    end loop;
+	populateCollection(ip1, arr1);
+	populateCollection(ip2, arr2);
 
-	for i in 1..length(ip2) loop
-    	arr2.EXTEND;
-		arr2(i):= substr(ip2, i, 1);
-    end loop;
-
-	for i in 1..arr1.COUNT loop
-        if arr1(i) <> arr2(i) then
-        c:= c + 1;
-		end if;
-    end loop;
-
-	dbms_output.put_line(power(2,c));
-
+	dbms_output.put_line(answer(arr1,arr2));
 END;
