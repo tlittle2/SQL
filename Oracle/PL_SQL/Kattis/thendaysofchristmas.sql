0fcedff86c1d3cfd9f2c0a75f4551e5faee893c3
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-01-12 11:42:47 -0500

    Update thendaysofchristmas.sql

--- Oracle/PL_SQL/Kattis/thendaysofchristmas.sql
+++ Oracle/PL_SQL/Kattis/thendaysofchristmas.sql
@@ -1,36 +1,39 @@
 DECLARE    
 	type t_arr is table of integer index by pls_integer;
-	arr t_arr := t_arr();
+	arr t_arr;
+	
 	ip integer:= 12; --user input
 
-	PROCEDURE processInput is
-    	begin
-    		for i in 1..ip loop
-        		arr(i):= i;
-        	end loop;
+	PROCEDURE processInput(p_ipString IN INTEGER, p_collection IN OUT t_arr) is
+	begin
+	        for i in 1..p_ipString loop
+	        	p_collection(i):= i;
+	        end loop;
     	end;
 
-	FUNCTION calculateOutput1(a IN t_arr) return INTEGER IS
+	FUNCTION calculateOutput1(p_collection IN t_arr) return INTEGER IS
     	out1 integer:= 0;
     	begin
-        	for i in arr.first..arr.last loop
-			out1:= out1 +arr(i);
+        	for i in p_collection.FIRST..p_collection.LAST loop
+			out1:= out1 + p_collection(i);
 		end loop;
 		
 		return out1;
     	end;
 
-	FUNCTION calculateOutput2(a IN t_arr) return INTEGER IS
+	FUNCTION calculateOutput2(p_ipString IN INTEGER) return INTEGER IS
     	out2 integer:= 0;
     	begin
-        	for i in 1..ip loop
+        	for i in 1..p_ipString loop
 	        	for j in 1..i loop
     				out2:= out2 + j;
         		end loop;
     		end loop;
 		return out2;
     	end;
+
 BEGIN
-	processInput;
-	dbms_output.put_line(calculateOutput1(arr) || ' ' || calculateOutput2(arr));
+	processInput(ip, arr);
+	dbms_output.put_line(calculateOutput1(arr));
+	dbms_output.put_line(calculateOutput2(ip));
 END;
