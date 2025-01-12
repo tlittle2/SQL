4519963b2a98c036d5b0624fc5d96ae2016b0e99
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-01-12 10:59:14 -0500

    Create licensetolaunch2.sql

---
+++ Oracle/PL_SQL/Kattis/License_To_Launch/licensetolaunch2.sql
@@ -0,0 +1,40 @@
+DECLARE
+    type t_nums is table of PLS_INTEGER index by PLS_INTEGER;
+	t_arr t_nums;
+	
+	type t_input is table of PLS_INTEGER;
+	t_ip t_input := t_input(3,4,1,7,2); --ip from user
+
+	minNum PLS_INTEGER;
+	minKey PLS_INTEGER;
+
+
+procedure populateMap(p_ip IN t_input, p_arr IN OUT t_nums) is
+begin
+    for i in p_ip.FIRST..p_ip.LAST
+    loop
+        	p_arr(i) := p_ip(i);
+    end loop;
+end;
+
+FUNCTION returnAnswer(p_arr IN t_nums) return PLS_INTEGER is
+begin
+    minNum := p_arr(t_arr.FIRST);
+	minKey := p_arr.FIRST;
+
+	for i in p_arr.FIRST..p_arr.LAST
+    loop
+        if p_arr(i) < minNum then
+        	minNum := t_arr(i);
+        	minKey := i;
+		end if;
+    end loop;
+
+	return minKey-1;
+end;
+
+BEGIN
+    populateMap(t_ip, t_arr);
+	dbms_output.put_line(returnAnswer(t_arr));
+
+END;
