1ca7d3e696c186c8bcb82508cb98a49f5b4e45f5
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-01-12 11:33:24 -0500

    Update helpaphd.sql

--- Oracle/PL_SQL/Kattis/helpaphd.sql
+++ Oracle/PL_SQL/Kattis/helpaphd.sql
@@ -1,31 +1,31 @@
 DECLARE
-  type t_arr is table of varchar2(12);
+	type t_arr is table of varchar2(12);
 	problems t_arr := t_arr('2+2','1+2','P=NP','0+0');	--user input
 
 	FUNCTION solveProblem(ip VARCHAR2) return INTEGER is
-    delimiter CHAR(1):= '+';
-    num1 integer := to_number(substr(ip, 0, instr(ip,delimiter)-1));
-	  num2 integer := to_number(substr(ip, instr(ip,delimiter), length(ip)));
-    BEGIN
-        return num1+num2;
-    END;
+		delimiter CHAR(1):= '+';
+		num1 integer := to_number(substr(ip, 0, instr(ip,delimiter)-1));
+		num2 integer := to_number(substr(ip, instr(ip,delimiter), length(ip)));
+    	BEGIN
+        	return num1+num2;
+    	END;
 
 	FUNCTION evaluateProblem(ip VARCHAR2) RETURN BOOLEAN IS
-    badIP CONSTANT VARCHAR2(4):= 'P=NP';
-    BEGIN
-        if ip = badIP then
-        	return True;
-		    else
-          return False;
-        end if;
-    END;
+		badIP CONSTANT VARCHAR2(4):= 'P=NP';
+	BEGIN
+		if ip = badIP then
+		return True;
+		else
+          	return False;
+        	end if;
+    	END;
 
 BEGIN    
     for i in problems.FIRST..problems.LAST LOOP
-		if evaluateProblem(problems(i)) then
-            dbms_output.put_line('SKIPPED');
-		else
-            dbms_output.put_line(solveProblem(problems(i)));
-		end if;
-	END LOOP;
+	if evaluateProblem(problems(i)) then
+		dbms_output.put_line('SKIPPED');
+	else
+        	dbms_output.put_line(solveProblem(problems(i)));
+	end if;
+     END LOOP;
 END;
