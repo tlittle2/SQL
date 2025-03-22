769d37b86f9b6d0de464ae75c3533b9613599240
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-03-22 13:00:05 -0400

    Update stafur.sql

--- Oracle/PL_SQL/Kattis/stafur/stafur.sql
+++ Oracle/PL_SQL/Kattis/stafur/stafur.sql
@@ -1,16 +1,16 @@
 DECLARE
-  type vowelList is table of char(1);
+	type vowelList is table of char(1);
 	vowels vowelList := vowelList('A','E','I','O','U');
 	ip char(1):= 'A';
 
 BEGIN
-  if ip member of vowels then
-    dbms_output.put_line('Jebb');
+	if ip member of vowels then
+    		dbms_output.put_line('Jebb');
 	else
-    if ip = 'Y' then
-      dbms_output.put_line('Kannski');
-    else
-      dbms_output.put_line('Neibb');
-    end if;
-  end if;
+    		if ip = 'Y' then
+      			dbms_output.put_line('Kannski');
+    		else
+      			dbms_output.put_line('Neibb');
+    		end if;
+  	end if;
 END;
