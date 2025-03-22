c3a0037b6e2db5d894162a287e4c3e84aa25256d
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-03-22 12:52:25 -0400

    Update apaxiaaans.sql

--- Oracle/PL_SQL/Kattis/apaxiaaans/apaxiaaans.sql
+++ Oracle/PL_SQL/Kattis/apaxiaaans/apaxiaaans.sql
@@ -1,24 +1,24 @@
 DECLARE
-  type v_arr is table of char(1);
+	type v_arr is table of char(1);
 	arr v_arr := v_arr();
 
 	ip varchar2(32767) := 'rooobert';
 	ans varchar(32767);
 
 BEGIN
-    for i in 1..length(ip) loop
-    	arr.EXTEND;
-		  arr(arr.LAST) := substr(ip, i, 1);
-    end loop;
+    	for i in 1..length(ip) loop
+		arr.EXTEND;
+		arr(arr.LAST) := substr(ip, i, 1);
+    	end loop;
 
-	for i in 1..arr.COUNT-1 LOOP
-        if arr(i) <> arr(i+1) then
-        ans:= ans || arr(i);
-        end if;
-    END LOOP;
+	for i in 1..arr.COUNT-1
+	LOOP
+	        if arr(i) <> arr(i+1) then
+	        ans:= ans || arr(i);
+	        end if;
+    	END LOOP;
 
 	ans:= ans || arr(arr.LAST);
-
 	dbms_output.put_line(ans);
 
 END;
