29ec77abd8569c889c8c983b1e2911d87cac2a65
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-03-22 13:26:21 -0400

    Update backspace.sql

--- Oracle/PL_SQL/Kattis/backspace/backspace.sql
+++ Oracle/PL_SQL/Kattis/backspace/backspace.sql
@@ -17,23 +17,28 @@ DECLARE
 	end;
 	
 BEGIN
-    for i in reverse 1..length(ip_str) loop
-    	if getCurrentChar(ip_str, i) = '<' then
-        	positions(i):= space_char;
-			    cnt:= cnt+1;
-		  elsif cnt > 0 then
-          positions(i):= space_char;
-			    cnt:= cnt-1;
-		  else
-          positions(i):= getCurrentChar(ip_str, i);
-        end if;
-    end loop;
+	for i in reverse 1..length(ip_str)
+	loop
+		if getCurrentChar(ip_str, i) = '<'
+		then
+			positions(i):= space_char;
+			cnt:= cnt+1;
+		elsif cnt > 0
+		then
+			positions(i):= space_char;
+			cnt:= cnt-1;
+		else
+			positions(i):= getCurrentChar(ip_str, i);
+		end if;
+	end loop;
 
 	--dbms_output.put() was not working in oracle live sql, so i appended to a string
-	for i in positions.FIRST..positions.LAST loop
-        if positions(i) <> space_char then
-        	ans:= ans || positions(i); 
+	for i in positions.FIRST..positions.LAST
+	loop
+		if positions(i) <> space_char
+		then
+        		ans:= ans || positions(i); 
 		end if;
-    end loop;
+    	end loop;
 	dbms_output.put_line(ans);
 END;
