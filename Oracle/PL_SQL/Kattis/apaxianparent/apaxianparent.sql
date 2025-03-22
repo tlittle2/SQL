fe47c3be7333f97f93a881803154cf3c8008064f
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-03-22 12:55:07 -0400

    Update apaxianparent.sql

--- Oracle/PL_SQL/Kattis/apaxianparent/apaxianparent.sql
+++ Oracle/PL_SQL/Kattis/apaxianparent/apaxianparent.sql
@@ -1,26 +1,22 @@
 DECLARE
-
-    type t_chars is table of char(1);
-
+	type t_chars is table of char(1);
 	vowels t_chars := t_chars('a','e','i','o', 'u');
-
-    ipString varchar2(20) := 'andrex naxos';
-    kid varchar2(10):= substr(ipString, 1, instr(ipString, ' ')-1);
+	ipString varchar2(20) := 'andrex naxos';
+	
+	kid varchar2(10):= substr(ipString, 1, instr(ipString, ' ')-1);
 	parent varchar2(10):= substr(ipString, instr(ipString, ' ')+1, length(ipString));
 
 	outString varchar2(20);
 	
 BEGIN
-    dbms_output.put_line(kid);
+	dbms_output.put_line(kid);
 	dbms_output.put_line(parent);
-    
 	case
-    	when substr(kid, length(kid),1) = 'e' then outString := kid || 'x' || parent;
+    		when substr(kid, length(kid),1) = 'e' then outString := kid || 'x' || parent;
 		when substr(kid, length(kid)-1,2) = 'ex' then outString := kid || parent;
 		when substr(kid, length(kid),1) member of vowels then outString:= substr(kid, 1,length(kid)- 1) || 'ex' || parent;
 		else outString := kid || 'ex' || parent;
-    end case;
+    	end case;
 
 	dbms_output.put_line(outString);
-
 END;
