987215ca08d0ba50a7ff4f9ea40f8162c9b408d1
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-03-22 13:29:00 -0400

    Update isyavowel.sql

--- Oracle/PL_SQL/Kattis/isyavowel/isyavowel.sql
+++ Oracle/PL_SQL/Kattis/isyavowel/isyavowel.sql
@@ -5,11 +5,13 @@ DECLARE
 	ans integer:= 0;
 	ans2 integer;
 BEGIN
-    for i in 1..length(word) loop
-    	if substr(word, i, 1) member of vowels then
-    	ans:= ans + 1;
-    	end if;
-    end loop;
+	for i in 1..length(word)
+	loop
+	    	if substr(word, i, 1) member of vowels then
+	    	ans:= ans + 1;
+	    	end if;
+    	end loop;
+	
 	ans2:= length(replace(word, 'y', '')) - ans;
 	dbms_output.put_line(ans || ' ' || ans2);
 END;
