842cf00db37e8a8fe40d88f20653ffc4289c90c2
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-03-22 13:24:47 -0400

    Update aaah.sql

--- Oracle/PL_SQL/Kattis/aaah/aaah.sql
+++ Oracle/PL_SQL/Kattis/aaah/aaah.sql
@@ -4,9 +4,8 @@ DECLARE
 	
 BEGIN
 	if length(ip1) >= length(ip2) then
-    	dbms_output.put_line('go');
-    else
-        dbms_output.put_line('no');
-    end if;
-
+		dbms_output.put_line('go');
+    	else
+        	dbms_output.put_line('no');
+    	end if;
 END;
