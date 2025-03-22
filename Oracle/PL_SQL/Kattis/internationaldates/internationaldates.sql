36d152a8dd4dd4687fe40f7bce9ccf3dfc2ba53d
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-03-22 13:28:28 -0400

    Update internationaldates.sql

--- Oracle/PL_SQL/Kattis/internationaldates/internationaldates.sql
+++ Oracle/PL_SQL/Kattis/internationaldates/internationaldates.sql
@@ -1,17 +1,15 @@
 DECLARE
-  ip varchar2(10) := '25/03/2023';
+	ip varchar2(10) := '25/03/2023';
 
 	v_part1 integer:= cast(substr(ip, 1, 2) as integer);
 	v_part2 integer:= cast(substr(ip, 4, 2) as integer);
 	v_year integer:= cast(substr(ip, 7, 4) as integer);
-		
 BEGIN
-    if v_part1 > 12 then
-    	dbms_output.put_line('EU');
+	if v_part1 > 12 then
+    		dbms_output.put_line('EU');
 	elsif v_part2 > 12 then
-      dbms_output.put_line('US');
+      		dbms_output.put_line('US');
 	else
-      dbms_output.put_line('either');
+      		dbms_output.put_line('either');
 	end if;
-
 END;
