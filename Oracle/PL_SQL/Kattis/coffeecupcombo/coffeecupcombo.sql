5bd260d0e669ea22b94cb1144117d4601e88cc4d
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-03-22 12:45:39 -0400

    Update coffeecupcombo.sql

--- Oracle/PL_SQL/Kattis/coffeecupcombo/coffeecupcombo.sql
+++ Oracle/PL_SQL/Kattis/coffeecupcombo/coffeecupcombo.sql
@@ -1,20 +1,20 @@
 DECLARE
-  ip varchar2(10):= '1100000000';
+	ip varchar2(10):= '1100000000';
 	ans INTEGER:= 0;
 	tempSum INTEGER:= 0;
 
 BEGIN
-    for i in 1..length(ip) loop
-    	if substr(ip, i, 1) = '1' then
-    		tempSum := 2;
+	for i in 1..length(ip) loop
+    		if substr(ip, i, 1) = '1' then
+	    		tempSum := 2;
 			ans := ans + 1;
-    	else
-            if tempSum > 0 then
-            ans := ans + 1;
-			tempSum := tempSum -1;
-            end if;
-    	end if;
-    end loop;
+    		else
+	            	if tempSum > 0 then
+	            		ans := ans + 1;
+				tempSum := tempSum -1;
+	            	end if;
+    		end if;
+    	end loop;
 
 	dbms_output.put_line(ans);
 
