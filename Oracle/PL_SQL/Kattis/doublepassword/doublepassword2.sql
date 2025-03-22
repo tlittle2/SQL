82543e6a6431756a74d5ec807d6f4302a3e56625
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-03-22 15:26:18 -0400

    Update doublepassword2.sql

--- Oracle/PL_SQL/Kattis/doublepassword/doublepassword2.sql
+++ Oracle/PL_SQL/Kattis/doublepassword/doublepassword2.sql
@@ -1,16 +1,12 @@
 with ip as(
     select 
-      '2718' as str
-    , '2718' as str2
+      '1111' as str
+    , '1234' as str2
     from dual
 )
-
-select power(2,sum(sameChar)) from(
-    select ip1, ip2, (case when ip1 <> ip2 then 1 else 0 end) as sameChar from (
-        select substr(str, level,1) as ip1
-        ,  substr(str2, level,1) as ip2
-        from ip
-        connect by level <= length(str)
-    )
-)
-;
+select power(2,sum(case when ip1 <> ip2 then 1 else 0 end)) from (
+    select substr(str, level,1) as ip1
+    ,  substr(str2, level,1) as ip2
+    from ip
+    connect by level <= length(str)
+);
