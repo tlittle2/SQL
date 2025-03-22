4b8f1740c23b29c402170afafcdbb9718a0e3303
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-03-22 13:30:29 -0400

    Update kornislav.sql

--- Oracle/PL_SQL/Kattis/kornislav/kornislav.sql
+++ Oracle/PL_SQL/Kattis/kornislav/kornislav.sql
@@ -1,8 +1,6 @@
-DECLARE    
+DECLARE
 	type t_arr is table of number;
 	ipArr t_arr := t_arr(4,4,3,4);
-
 BEGIN
-    dbms_output.put_line(ipArr(1) * ipArr(3));
-    
+	dbms_output.put_line(ipArr(1) * ipArr(3));
 END;
