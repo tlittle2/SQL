f618ad09b864e099e87bf0b7ef0e782ff92f1f1d
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-03-22 13:30:03 -0400

    Update knightpacking.sql

--- Oracle/PL_SQL/Kattis/knightpacking/knightpacking.sql
+++ Oracle/PL_SQL/Kattis/knightpacking/knightpacking.sql
@@ -2,15 +2,13 @@ DECLARE
 	ip number := 1;
 
 	function answer(p_ip number) return VARCHAR2 is
-    begin
-        if mod(p_ip, 2) = 1 then
-    	return 'first';
-    	else
-		return 'second';
-    	end if;
-    end;
-
+	begin
+	        if mod(p_ip, 2) = 1 then
+	    		return 'first';
+	    	else
+			return 'second';
+	    	end if;
+    	end;
 BEGIN
     dbms_output.put_line(answer(ip));
-	
 END;
