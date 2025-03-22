5b1aca023df8d8dd00f57d1bf3475847ceafe4c8
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-03-22 18:43:36 -0400

    Update champernowneverification.sql

--- Oracle/SQL/Kattis/Champernowne Verification/champernowneverification.sql
+++ Oracle/SQL/Kattis/Champernowne Verification/champernowneverification.sql
@@ -1,9 +1,9 @@
 with ip as(
-    select '1000000000' as ip1 from dual
+    select '123456789' as ip1 from dual
     
 )
 
-select case when min(ans) = -1 then -1 else max(ans) end as answer from (
+select decode(min(ans), -1, -1, max(ans)) as answer from (
     select rn, ip1, val, case when rn = val then rn else -1 end as ans from (
         select rownum as rn, ip1, to_number(substr(ip1, level, 1)) as val
         from ip 
