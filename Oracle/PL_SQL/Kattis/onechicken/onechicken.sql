2e30edb36d91f4dcf177445ccdd5361c454ed4e5
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-03-22 12:56:16 -0400

    Update onechicken.sql

--- Oracle/PL_SQL/Kattis/onechicken/onechicken.sql
+++ Oracle/PL_SQL/Kattis/onechicken/onechicken.sql
@@ -5,17 +5,17 @@ DECLARE
 	m integer:= to_number(substr(ip, instr(ip, ' ')+1, length(ip)));
 			
 BEGIN
-    if n < m then
-    	if m - n = 1 then
-    		dbms_output.put_line('Dr. Chaz will have '|| (m-n) || ' piece of chicken left over!');
+	if n < m then
+    		if m - n = 1 then
+    			dbms_output.put_line('Dr. Chaz will have '|| (m-n) || ' piece of chicken left over!');
 		else
-            dbms_output.put_line('Dr. Chaz will have '|| (m-n) || ' pieces of chicken left over!');
-        end if;
+            		dbms_output.put_line('Dr. Chaz will have '|| (m-n) || ' pieces of chicken left over!');
+        	end if;
 	else
-        if n - m = 1 then
-    		dbms_output.put_line('Dr. Chaz needs '|| (n-m) || ' more piece of chicken!');
+		if n - m = 1 then
+    			dbms_output.put_line('Dr. Chaz needs '|| (n-m) || ' more piece of chicken!');
 		else
-            dbms_output.put_line('Dr. Chaz needs '|| (n-m) || ' more pieces of chicken!');
-        end if;
-    end if;
+            		dbms_output.put_line('Dr. Chaz needs '|| (n-m) || ' more pieces of chicken!');
+        	end if;
+    	end if;
 END;
