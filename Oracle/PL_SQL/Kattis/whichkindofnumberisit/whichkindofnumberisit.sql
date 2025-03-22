4e5cc8288616519c155e77ad34c3a28840602244
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-03-22 13:40:19 -0400

    Update whichkindofnumberisit.sql

--- Oracle/PL_SQL/Kattis/whichkindofnumberisit/whichkindofnumberisit.sql
+++ Oracle/PL_SQL/Kattis/whichkindofnumberisit/whichkindofnumberisit.sql
@@ -6,36 +6,39 @@ DECLARE
 
 	FUNCTION isSquare(n INTEGER) RETURN BOOLEAN IS
     	sqrt_value number:= sqrt(n);
-    	BEGIN
-	        if trunc(sqrt_value) = sqrt_value then
-	        return True;
+    	begin
+	        if trunc(sqrt_value) = sqrt_value
+		then
+	        	return True;
 	        end if;
 	    	return False;
-    	END;
+    	end;
 
 	FUNCTION isOdd(n INTEGER) RETURN BOOLEAN IS
-    	BEGIN
-	        if mod(n,2) = 1 then
-	        return True;
+    	begin
+	        if mod(n,2) = 1
+		then
+	        	return True;
 	        end if;
 	    	return False;
-    	END;
+    	end;
 
 	PROCEDURE displayAnswer(str VARCHAR2) is
-    	BEGIN
-	        if length(str) > 0 then
-	        dbms_output.put_line(ans);
-			else
-	        dbms_output.put_line('EMPTY');
+    	begin
+		if length(str) > 0 then
+	        	dbms_output.put_line(ans);
+		else
+	        	dbms_output.put_line('EMPTY');
 	        end if;
-		
+
 		ans:= '';
-	END;
+	end;
 
 BEGIN
 	for i in numbers.FIRST..numbers.LAST
 	loop
-		if isOdd(numbers(i)) then
+		if isOdd(numbers(i))
+		then
 			ans:= ans || 'O';
 		end if;
 		
