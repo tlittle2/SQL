54b9bd45dbf03c987a3b6f21b61d9c97e77fa556
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-03-22 13:26:45 -0400

    Update canadianseh.sql

--- Oracle/PL_SQL/Kattis/canadianseh/canadianseh.sql
+++ Oracle/PL_SQL/Kattis/canadianseh/canadianseh.sql
@@ -1,11 +1,10 @@
 DECLARE
-  sentence varchar(100):= 'Im not suspicious, Eh?';
+	sentence varchar(100):= 'Im not suspicious, Eh?';
 
 BEGIN
-    if substr(sentence, -3) = 'eh?' then
+	if substr(sentence, -3) = 'eh?' then
 		dbms_output.put_line('Canadian!');
-    else
-    	dbms_output.put_line('Imposter!');
-    end if;
-
+	else
+		dbms_output.put_line('Imposter!');
+	end if;
 END;
