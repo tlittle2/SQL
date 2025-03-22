0bd486daa59efbb46e1144314ad8e0177ae43470
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-03-22 12:56:31 -0400

    Update planina.sql

--- Oracle/PL_SQL/Kattis/planina/planina.sql
+++ Oracle/PL_SQL/Kattis/planina/planina.sql
@@ -1,5 +1,5 @@
 DECLARE
-  type t_arr is table of integer;
+	type t_arr is table of integer;
 	numbers t_arr := t_arr(1,7,20,9);
 	ip1 integer:= 5; --user input 1
 	ans integer:= power((power(2, ip1)+1),2);
