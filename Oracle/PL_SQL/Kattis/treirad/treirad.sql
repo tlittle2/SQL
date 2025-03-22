02766583aa11ca67fdf24ce464ace9e22e75f488
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-03-22 13:01:36 -0400

    Update treirad.sql

--- Oracle/PL_SQL/Kattis/treirad/treirad.sql
+++ Oracle/PL_SQL/Kattis/treirad/treirad.sql
@@ -1,15 +1,14 @@
 DECLARE
-    ip integer:= 1234;
-
+	ip integer:= 1234;
 	strt integer:= 1;
 	cnt integer:=0;		
 
 BEGIN
-    loop
-      exit when strt * (strt + 1) * (strt + 2) >= ip;
-      cnt:= cnt+1;
-  	  strt:= strt+1;
-	  end loop;
+	loop
+		exit when strt * (strt + 1) * (strt + 2) >= ip;
+      		cnt:= cnt+1;
+  	  	strt:= strt+1;
+	end loop;
 
 	dbms_output.put_line(cnt);
 	
