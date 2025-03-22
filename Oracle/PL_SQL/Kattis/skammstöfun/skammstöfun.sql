184deb7caf9fbe87811ceabfd74f8acc11319ab9
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-03-22 12:59:11 -0400

    Update skammstöfun.sql

--- Oracle/PL_SQL/Kattis/skammstöfun/skammstöfun.sql
+++ Oracle/PL_SQL/Kattis/skammstöfun/skammstöfun.sql
@@ -1,23 +1,25 @@
 DECLARE
-    type v_arr is table of char(1);
+	type v_arr is table of char(1);
 	arr v_arr := v_arr();
 
 	ip varchar2(100) := 'GNU is Not Unix'; --user input
 	ans varchar(100):= substr(ip, 1, 1); --first letter will always be upper case?
 
 BEGIN
-    for i in 1..length(ip) loop
-    	arr.EXTEND;
+	for i in 1..length(ip)
+	loop
+		arr.EXTEND;
 		arr(arr.LAST) := substr(ip, i, 1);
-    end loop;
+    	end loop;
 
-	for i in 1..arr.COUNT LOOP
-        if arr(i) = ' ' then
-        	if arr(i+1) = upper(arr(i+1)) then
-        	ans:= ans || arr(i+1);
-		end if;
-        end if;
-    END LOOP;
+	for i in 1..arr.COUNT
+	LOOP
+        	if arr(i) = ' ' then
+	        	if arr(i+1) = upper(arr(i+1)) then
+	        	ans:= ans || arr(i+1);
+			end if;
+        	end if;
+    	END LOOP;
 
 	dbms_output.put_line(ans);
 END;
