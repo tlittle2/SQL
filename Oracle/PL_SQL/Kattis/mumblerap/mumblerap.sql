3c0f9ff8b21370ddc44e5f96779a84e65aca110d
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-03-22 13:31:22 -0400

    Update mumblerap.sql

--- Oracle/PL_SQL/Kattis/mumblerap/mumblerap.sql
+++ Oracle/PL_SQL/Kattis/mumblerap/mumblerap.sql
@@ -8,23 +8,21 @@ DECLARE
 	currMax integer:= -1;
 
 	procedure processInput is
-    begin
-        for i in 1..REGEXP_COUNT(ip_str, '\d+') loop
-    	num_match := REGEXP_SUBSTR(ip_str, '\d+', 1, i);
-    	if num_match is not null then
-    		arr.EXTEND;
-    		arr(arr.COUNT):= to_number(num_match);
-		end if;
-    end loop;
-    end;
-
-
+	begin
+	        for i in 1..REGEXP_COUNT(ip_str, '\d+')
+		loop
+		    	num_match := REGEXP_SUBSTR(ip_str, '\d+', 1, i);
+		    	if num_match is not null then
+				arr.EXTEND;
+		    		arr(arr.COUNT):= to_number(num_match);
+			end if;
+	    	end loop;
+    	end;
 BEGIN
 	processInput;
 	for i in arr.FIRST..arr.LAST loop
-        currMax:= greatest(currMax, arr(i));
-    end loop;
+        	currMax:= greatest(currMax, arr(i));
+    	end loop;
 
 	dbms_output.put_line(currMax);
-
 END;
