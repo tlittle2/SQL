9c692a4f020ed8886bd208b51f591ec926eb36ad
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-03-22 12:41:07 -0400

    Update quickbrownfox2.sql

--- Oracle/PL_SQL/Kattis/quickbrownfox2/quickbrownfox2.sql
+++ Oracle/PL_SQL/Kattis/quickbrownfox2/quickbrownfox2.sql
@@ -7,40 +7,40 @@ DECLARE
 	ip varchar2(100) := 'ZYXW, vu TSR Ponm lkj ihgfd CBA.';
 
 	procedure getSetOfCharacters(p_chrset IN OUT char_arr_t) is
-    begin
-        for i in ascii('a')..ascii('z') loop --97-122
-    	p_chrset.EXTEND;
-		p_chrset(i - ascii('a') + 1):= chr(i); --indices 1-26
-		end loop;
-    end;
+    	begin
+	        for i in ascii('a')..ascii('z') loop --97-122
+	    	p_chrset.EXTEND;
+			p_chrset(i - ascii('a') + 1):= chr(i); --indices 1-26
+			end loop;
+    	end;
 
 	procedure getCharactersfromInput(p_ipString IN VARCHAR2, p_c_arr IN char_arr_t, p_ip_arr IN OUT char_arr_t) is 
-    begin
-        for i in 1..length(p_ipString) loop
-        if lower(substr(p_ipString, i, 1)) member of p_c_arr then
-        	p_ip_arr.EXTEND;
-    		p_ip_arr(p_ip_arr.LAST):= lower(substr(p_ipString, i, 1));
-		end if;
-	end loop;
-    end;
+    	begin
+	        for i in 1..length(p_ipString) loop
+	        if lower(substr(p_ipString, i, 1)) member of p_c_arr then
+	        	p_ip_arr.EXTEND;
+	    		p_ip_arr(p_ip_arr.LAST):= lower(substr(p_ipString, i, 1));
+			end if;
+		end loop;
+    	end;
 
 	procedure computeMissing(p_c_arr IN char_arr_t, p_ip_arr IN char_arr_t, p_outString IN OUT VARCHAR2) IS
-    BEGIN
-        for i in 1..p_c_arr.COUNT loop
-        if p_c_arr(i) not member of p_ip_arr then
-        p_outString:= p_outString || p_c_arr(i);
-        end if;
+    	BEGIN
+	        for i in 1..p_c_arr.COUNT loop
+	        if p_c_arr(i) not member of p_ip_arr then
+	        p_outString:= p_outString || p_c_arr(i);
+	        end if;
 		end loop;
-    END;
+    	END;
 
 	function isPanagram(p_ip_string VARCHAR2) RETURN VARCHAR2 IS
-    BEGIN
-        if length(p_ip_string) is null then
+    	BEGIN
+	        if length(p_ip_string) is null then
 			RETURN 'pangram';
-    	else
-            RETURN 'missing ' || missing;
+	    	else
+	            RETURN 'missing ' || missing;
 		end if;
-    END;
+    	END;
 
 
 BEGIN
