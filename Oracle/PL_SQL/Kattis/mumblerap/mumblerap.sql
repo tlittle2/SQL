cc5aed3dcf4c8630f4c0213d395810867f30172d
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-05-24 16:57:39 -0400

    Update mumblerap.sql

--- Oracle/PL_SQL/Kattis/mumblerap/mumblerap.sql
+++ Oracle/PL_SQL/Kattis/mumblerap/mumblerap.sql
@@ -1,29 +1,39 @@
-DECLARE    
+DECLARE
+	subtype max_varchar2 is varchar2(32767);
+	ip_str max_varchar2 := 'yesterdayihad1001,BUTnowihave9999'; --user input
+	
 	type t_arr is table of number;
 	arr t_arr := t_arr();
 
-	subtype max_varchar2 is varchar2(32767);
-	ip_str max_varchar2 := 'yesterdayihad1001,BUTnowihave9999'; --user input
-	num_match max_varchar2;
-	currMax integer:= -1;
-
-	procedure processInput
+	procedure processInput(p_ipStr IN max_varchar2, p_arr IN OUT NOCOPY t_arr)
 	is
+        	num_match max_varchar2;
 	begin
-	        for i in 1..REGEXP_COUNT(ip_str, '\d+')
+		for i in 1..REGEXP_COUNT(p_ipStr, '\d+')
 		loop
-		    	num_match := REGEXP_SUBSTR(ip_str, '\d+', 1, i);
-		    	if num_match is not null then
-				arr.EXTEND;
-		    		arr(arr.COUNT):= to_number(num_match);
+			num_match := REGEXP_SUBSTR(p_ipStr, '\d+', 1, i);
+            		if num_match is not null
+            		then
+                		p_arr.EXTEND;
+                		p_arr(arr.COUNT):= to_number(num_match);
 			end if;
 	    	end loop;
-    	end;
-BEGIN
-	processInput;
-	for i in arr.FIRST..arr.LAST loop
-        	currMax:= greatest(currMax, arr(i));
-    	end loop;
+	end;
 
-	dbms_output.put_line(currMax);
+	function findMax(p_arr IN t_arr)
+    	return integer
+    	is
+        	currMax integer:= -1;
+    	begin
+	        for i in arr.FIRST..arr.LAST
+	        loop
+	            currMax:= greatest(currMax, p_arr(i));
+	        end loop;
+
+        	return currMax;
+    	end;
+
+BEGIN
+	processInput(ip_str, arr);
+	dbms_output.put_line(findMax(arr));
 END;
