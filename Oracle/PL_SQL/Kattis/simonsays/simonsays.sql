a0d3212d646a50941911d091b365183ac4b4b89a
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-05-24 16:18:48 -0400

    Update simonsays.sql

--- Oracle/PL_SQL/Kattis/simonsays/simonsays.sql
+++ Oracle/PL_SQL/Kattis/simonsays/simonsays.sql
@@ -1,5 +1,5 @@
 DECLARE
-	type v_arr is table of varchar(100);
+	type v_arr is table of varchar2(100);
 	arr v_arr := v_arr('Raise your right hand.','Lower your right hand.','Simon says raise your left hand.');
 	saying char(10) := 'Simon says';
 
