96cccf8837830c7970f4b0210023d11a7e8ccbf1
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-05-24 16:02:52 -0400

    Update composedrhythms.sql

--- Oracle/PL_SQL/Kattis/composedrhythms/composedrhythms.sql
+++ Oracle/PL_SQL/Kattis/composedrhythms/composedrhythms.sql
@@ -4,47 +4,49 @@ DECLARE
 
 	ip integer := 25; --user input
 
-	procedure addValue(a in out t_arr, val integer ) is
-    begin
-		  a.EXTEND;
-    	a(a.LAST):= val;
-    end;
+	procedure addValue(a in out t_arr, val integer)
+	is
+    	begin
+		a.EXTEND;
+    		a(a.LAST):= val;
+    	end;
 
-	procedure calculateOutput is
-    begin
-        loop
-          exit when ip <= 5;
-  		    addValue(arr, 3);
-      	  ip:= ip-3;
-		    end loop;
+	procedure calculateOutput
+	is
+    	begin
+	        loop
+	          exit when ip <= 5;
+	  		    addValue(arr, 3);
+	      	  ip:= ip-3;
+			    end loop;
+	
+	        if ip = 5 then
+	          addValue(arr, 3);
+	    		  addValue(arr, 2);
+	      	elsif ip=4 then
+	            addValue(arr, 2);
+	      		  addValue(arr, 2);
+	      	elsif ip=3 then
+	            addValue(arr, 3);
+	      	elsif ip=2 then
+	            addValue(arr, 2);
+	      	end if;
 
-        if ip = 5 then
-          addValue(arr, 3);
-    		  addValue(arr, 2);
-      	elsif ip=4 then
-            addValue(arr, 2);
-      		  addValue(arr, 2);
-      	elsif ip=3 then
-            addValue(arr, 3);
-      	elsif ip=2 then
-            addValue(arr, 2);
-      	end if;
+    	end;
 
-    end;
-
-	procedure displayOutput is
-    begin
-      dbms_output.put_line(arr.COUNT);
-    	dbms_output.put_line(lpad('-', length(arr.COUNT) *4, '-'));
-    	for i in arr.FIRST..arr.LAST
-            LOOP
-            dbms_output.put_line(arr(i));
-            END LOOP;
-    end;
+	procedure displayOutput
+	is
+    	begin
+		dbms_output.put_line(arr.COUNT);
+		dbms_output.put_line(lpad('-', length(arr.COUNT) *4, '-'));
+    		for i in arr.FIRST..arr.LAST
+            	LOOP
+            		dbms_output.put_line(arr(i));
+            	END LOOP;
+    	end;
     	
 
 BEGIN
-  calculateOutput;
+  	calculateOutput;
 	displayOutput;
-
 END;
