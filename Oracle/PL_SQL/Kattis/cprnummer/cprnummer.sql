31a4b87a584766ea8e3725df402219582161825a
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-05-24 16:03:43 -0400

    Update cprnummer.sql

--- Oracle/PL_SQL/Kattis/cprnummer/cprnummer.sql
+++ Oracle/PL_SQL/Kattis/cprnummer/cprnummer.sql
@@ -6,7 +6,8 @@ DECLARE
 
 	ip varchar2(11):= '310111-0469'; --user input
 
-	procedure processInput(p_ipString VARCHAR2, p_ipArr IN OUT v_arr) is
+	procedure processInput(p_ipString VARCHAR2, p_ipArr IN OUT v_arr)
+	is
 	begin
 	    p_ipArr := v_arr();
 	    for i in 1..length(p_ipString) loop
@@ -17,7 +18,8 @@ DECLARE
 	    end loop;
 	end;
 	
-	procedure processAnswer(p_ipArr IN v_arr, p_ansArr IN OUT v_arr) is
+	procedure processAnswer(p_ipArr IN v_arr, p_ansArr IN OUT v_arr)
+	is
 	begin
 	    p_ansArr := v_arr();
 	    for i in 1..p_ipArr.COUNT loop
@@ -26,7 +28,9 @@ DECLARE
 	    end loop;
 	end;
 	
-	function findFinal(p_ansArr IN v_arr) return integer is
+	function findFinal(p_ansArr IN v_arr)
+	return integer
+	is
 	final integer := 0;
 	begin
 		for i in 1..ans_arr.COUNT
@@ -35,15 +39,14 @@ DECLARE
 	    	end loop;
 		return mod(final, 11);
 	end;
-	
 BEGIN
 	processInput(ip, ip_arr);
 	processAnswer(ip_arr, ans_arr);
 
-	if findFinal(ans_arr) = 0 then
-        dbms_output.put_line(1);
+	if findFinal(ans_arr) = 0
+	then
+        	dbms_output.put_line(1);
 	else
-        dbms_output.put_line(0);
+        	dbms_output.put_line(0);
    	end if;
-
 END;
