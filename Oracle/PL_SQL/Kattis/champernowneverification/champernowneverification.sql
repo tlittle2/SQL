d5941e34cecdad23de9a3b42411b45e1d67f1abc
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-05-24 15:59:35 -0400

    Update champernowneverification.sql

--- Oracle/PL_SQL/Kattis/champernowneverification/champernowneverification.sql
+++ Oracle/PL_SQL/Kattis/champernowneverification/champernowneverification.sql
@@ -2,8 +2,10 @@ DECLARE
 	type t_arr is table of integer index by pls_integer;
 	ip_str integer := 1000000000; --user input
 	
-	function createMasterList return t_arr is
-        mstr t_arr := t_arr();
+	function createMasterList
+	return t_arr
+	is
+        	mstr t_arr := t_arr();
     	begin
 	        for i in 1..length(ip_str) loop
 			mstr(i):= i;
@@ -12,7 +14,9 @@ DECLARE
 		return mstr;
     	end;
 
-	function findDifference(p_mstr t_arr) return BOOLEAN is
+	function findDifference(p_mstr t_arr)
+	return BOOLEAN
+	is
     	begin
 	        for i in 1..length(ip_str) loop
 		        if substr(ip_str, i, 1) <> p_mstr(i)
