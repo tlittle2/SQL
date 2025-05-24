2869d8bbda5bcb8b3e1ca38902b591b30615d978
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-05-24 16:42:30 -0400

    Update chardonnay.sql

--- Oracle/PL_SQL/Kattis/chardonnay/chardonnay.sql
+++ Oracle/PL_SQL/Kattis/chardonnay/chardonnay.sql
@@ -2,14 +2,17 @@
     	ip integer := 7; --value from standard input
 
     BEGIN
-        if ip = 0 then
+        if ip = 0
+        then
         	dbms_output.put_line(0);
         else
-        	if ip <> 7 then
-        	dbms_output.put_line(ip+1);
+        	if ip <> 7
+            then
+        	    dbms_output.put_line(ip+1);
         	else
-        	dbms_output.put_line(7);
+        	    dbms_output.put_line(7);
         	end if;
+        
         end if;
     
     END;
