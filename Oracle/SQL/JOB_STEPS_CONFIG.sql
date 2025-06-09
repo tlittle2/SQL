8ba2d2c120eb56b4e3c9c5f7bfe4d5cf71ab5cc2
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-06-09 19:23:41 -0400

    Update JOB_STEPS_CONFIG.sql

--- Oracle/SQL/JOB_STEPS_CONFIG.sql
+++ Oracle/SQL/JOB_STEPS_CONFIG.sql
@@ -1,6 +1,6 @@
 CREATE TABLE JOB_STEPS_CONFIG(
-  JOB_STEPS_CONFIG_SK NUMBER GENERATED ALWAYS AS IDENTITY  
-, SYSTEM_SK VARCHAR2(3 BYTE)
+  --JOB_STEPS_CONFIG_SK NUMBER GENERATED ALWAYS AS IDENTITY,
+  SYSTEM_SK VARCHAR2(3 BYTE)
 , SUBSYSTEM_SK VARCHAR2(3 BYTE)
 , JOB_ID NUMBER(10,0)
 , STEP_NUMBER NUMBER(10,0)
@@ -19,24 +19,35 @@ CREATE TABLE JOB_STEPS_CONFIG(
 
 
 
-INSERT INTO JOB_STEPS_CONFIG VALUES('000', '000', 1,2, 'P', 'BATCH_CDC', 'p_table_owner', 'INFA_SRC', 1, 1, 'Y', 'N', SYSTIMESTAMP, USER);
-INSERT INTO JOB_STEPS_CONFIG VALUES('000', '000', 1,2, 'P', 'BATCH_CDC', 'p_stage_table ', 'SALARY_DATA_STG', 1, 2, 'Y', 'N', SYSTIMESTAMP, USER);
-INSERT INTO JOB_STEPS_CONFIG VALUES('000', '000', 1,2, 'P', 'BATCH_CDC', 'p_cdc_table', 'SALARY_DATA_CDC', 1, 3, 'Y', 'N', SYSTIMESTAMP, USER);
-INSERT INTO JOB_STEPS_CONFIG VALUES('000', '000', 1,2, 'P', 'BATCH_CDC', 'p_target_table', 'SALARY_DATA', 1, 4, 'Y', 'N', SYSTIMESTAMP, USER);
+INSERT INTO JOB_STEPS_CONFIG VALUES('000', '000', 1,2, 'P', 'BATCH_CDC', 'p_job_nbr', '1', 1, 1, 'Y', 'N', SYSTIMESTAMP, USER);
 
 
 INSERT INTO JOB_STEPS_CONFIG VALUES('000', '000', 1,1, 'L', 'SQLLDR', 'p_control_file_path', '/prod/sqlldrs/', 1, 1, 'Y', 'N', SYSTIMESTAMP, USER);
 INSERT INTO JOB_STEPS_CONFIG VALUES('000', '000', 1,1, 'L', 'SQLLDR', 'p_control_file_name', 'SALARY_DATA_STG.ctl', 1, 2, 'Y', 'N', SYSTIMESTAMP, USER);
-INSERT INTO JOB_STEPS_CONFIG VALUES('000', '000', 1,1, 'L', 'SQLLDR', 'p_target_table', 'SALARY_DATA_STG', 2, 1, 'Y', 'N', SYSTIMESTAMP, USER);
 
 
 select * from job_steps_config where job_id = :p_job_id
 and active_flag = 'Y' and process_completed = 'N'
 order by step_number, operation_order, operation_sub_order;
 
---put arguments together
-select listagg(operation_value || ',') within group (order by operation_order, operation_sub_order) from job_steps_config
-where job_id = 1
-and active_flag = 'Y' and process_completed = 'N'
-and step_number = 2
-order by step_number, operation_order, operation_sub_order;
+
+with op as (
+select distinct op.operation_name as op_name
+from job_steps_config op
+where op.active_flag = 'Y' and op.process_completed = 'N'
+and job_id = 1
+and op.step_number = 2
+and op.operation_order = 1
+
+)
+, parms as (
+select listagg(operation_value || ',') within group (order by operation_order, operation_sub_order) as parm
+from job_steps_config op
+where op.active_flag = 'Y' and op.process_completed = 'N'
+and job_id = 1
+and op.step_number = 2
+and op.operation_order = 1
+order by operation_sub_order
+)
+
+select op.op_name || '(' || substr(parms.parm, 1,length(parms.parm) -1) || ')' as cmd from op, parms;
