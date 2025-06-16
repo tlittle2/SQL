c4b9aa52349711980f72b99d2700f87da9835d81
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-06-16 19:43:45 -0400

    Create apaxianparent2.sql

---
+++ Oracle/PL_SQL/Kattis/apaxianparent/apaxianparent2.sql
@@ -0,0 +1,28 @@
+DECLARE
+  mx_len CONSTANT NUMBER := 20;
+  subtype io_str_st is varchar2(mx_len);
+  subtype kp_str_st is varchar2(mx_len/2);
+
+  type t_chars is table of char(1);
+	vowels t_chars := t_chars('a','e','i','o', 'u');
+
+	ipString io_str_st := 'andrex naxos';
+	
+	kid kp_str_st:= substr(ipString, 1, instr(ipString, ' ')-1);
+	parent kp_str_st := substr(ipString, instr(ipString, ' ')+1, length(ipString));
+
+	outString io_str_st;
+	
+BEGIN
+	dbms_output.put_line(kid);
+	dbms_output.put_line(parent);
+	case
+    when substr(kid, length(kid),1) = 'e' then outString := kid || 'x' || parent;
+		when substr(kid, length(kid)-1,2) = 'ex' then outString := kid || parent;
+		when substr(kid, length(kid),1) member of vowels then outString:= substr(kid, 1,length(kid)- 1) || 'ex' || parent;
+		else outString := kid || 'ex' || parent;
+  end case;
+
+	dbms_output.put_line(outString);
+
+END;
