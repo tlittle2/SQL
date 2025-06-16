d0c2c4d062d068d4667af2848008572bd0409594
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-06-16 19:57:29 -0400

    Create alphabetical_aristocrats2.sql

---
+++ Oracle/PL_SQL/Kattis/Alphabetical_Aristocrats/alphabetical_aristocrats2.sql
@@ -0,0 +1,26 @@
+DECLARE
+    subtype nameLength_st is VARCHAR2(50);
+    type names_ip_t is table of nameLength_st;
+
+    --names_ip names_ip_t := names_ip_t('van der Steen' ,'fakederSteenOfficial' ,'Groot Koerkamp' ,'Bakker' ,'van den Hecken the Younger' ,'de Waal' ,'vant Hek');
+    names_ip names_ip_t := names_ip_t('var Emreis' ,'an Gleanna' ,'Terzieff Godefroy' ,'aep Ceallach' ,'of Rivia');
+
+    type output_t is table of nameLength_st index by nameLength_st;
+    ans output_t;
+
+    l_index nameLength_st;
+
+BEGIN
+    for i in names_ip.FIRST..names_ip.LAST
+    LOOP
+        ans(substr(names_ip(i), regexp_instr(names_ip(i), '[A-Z]'))) := names_ip(i);
+    END LOOP;
+
+    l_index := ans.FIRST;
+    loop
+        exit when l_index is null;
+        dbms_output.put_line(ans(l_index));
+        l_index := ans.NEXT(l_index);
+    END LOOP;
+
+END;
