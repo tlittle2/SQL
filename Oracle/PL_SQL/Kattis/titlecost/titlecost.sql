c89bd047eed0f2fe76d42b93fab0ebbfe2019b79
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-06 15:46:28 -0400

    Update titlecost.sql

--- Oracle/PL_SQL/Kattis/titlecost/titlecost.sql
+++ Oracle/PL_SQL/Kattis/titlecost/titlecost.sql
@@ -1,15 +1,15 @@
 DECLARE
-	ip varchar2(100) := 'PieHard 3.14159265358979323846';
+    ip varchar2(100) := 'PieHard 3.14159265358979323846';
 
-	ip_title VARCHAR2(100) := SUBSTR(ip, 1, INSTR(ip, ' ') - 1);
-  	ip_cost FLOAT(10) := CAST(TRIM(SUBSTR(ip, INSTR(ip, ' ') + 1)) AS FLOAT);
+    ip_title VARCHAR2(100) := substr(ip, 1, instr(ip, ' ') - 1);
+    ip_cost FLOAT(10) := cast(trim(substr(ip, instr(ip, ' ') + 1)) as float);
+    title_length INTEGER := length(ip_title);    
 
-  	title_length INTEGER := LENGTH(ip_title);	
 BEGIN
-	IF ip_cost < title_length
-	THEN
-        	DBMS_OUTPUT.PUT_LINE(ip_cost);
-    	ELSE
-        	DBMS_OUTPUT.PUT_LINE(title_length);
-    	END IF;
+    IF ip_cost < title_length
+    THEN
+        dbms_output.put_line(ip_cost);
+    ELSE
+        dbms_output.put_line(title_length);
+    END IF;
 END;
