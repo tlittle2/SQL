187d2540b0618c5af2f81852258a93435bc755cf
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-06 14:54:34 -0400

    Update licensetolaunch1.sql

--- Oracle/PL_SQL/Kattis/License_To_Launch/licensetolaunch1.sql
+++ Oracle/PL_SQL/Kattis/License_To_Launch/licensetolaunch1.sql
@@ -1,6 +1,6 @@
 DECLARE
-	type t_nums is table of PLS_INTEGER index by PLS_INTEGER;
-	t_arr constant t_nums:= t_nums(
+    type t_nums is table of PLS_INTEGER index by PLS_INTEGER;
+    t_arr constant t_nums:= t_nums(
         1 => 3,
         2 => 4,
         3 => 1,
@@ -8,22 +8,23 @@ DECLARE
         5 => 2
     );
 
-	minNum PLS_INTEGER;
-	minKey PLS_INTEGER;
+    minNum PLS_INTEGER;
+    minKey PLS_INTEGER;
 
 BEGIN
 
-	minNum := t_arr(t_arr.FIRST);
-	minKey := t_arr.FIRST;
+    minNum := t_arr(t_arr.FIRST);
+    minKey := t_arr.FIRST;
 
-	for i in t_arr.FIRST..t_arr.LAST
-    	loop
-        	if t_arr(i) < minNum then
-	        	minNum := t_arr(i);
-	        	minKey := i;
-		end if;
-    	end loop;
+    for i in t_arr.FIRST..t_arr.LAST
+    loop
+        if t_arr(i) < minNum
+        then
+            minNum := t_arr(i);
+            minKey := i;
+        end if;
+    end loop;
 
-	dbms_output.put_line(minKey-1);
+    dbms_output.put_line(minKey-1);
 
 END;
