099c495724655b1749f45ba008afe935057ca042
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-06 15:35:25 -0400

    Update quickbrownfox.sql

--- Oracle/PL_SQL/Kattis/quickbrownfox/quickbrownfox.sql
+++ Oracle/PL_SQL/Kattis/quickbrownfox/quickbrownfox.sql
@@ -1,45 +1,43 @@
-
 DECLARE
-	type char_arr_t is table of varchar(1);
-	c_arr char_arr_t:= char_arr_t();
-	ip_arr char_arr_t:= char_arr_t();
-	missing varchar(26);
+    type char_arr_t is table of varchar(1);
+    c_arr char_arr_t:= char_arr_t();
+    ip_arr char_arr_t:= char_arr_t();
+    missing varchar(26);
 
-	ip varchar2(100) := 'The quick brown fox jumps over the lazy dog.'; --input
+    ip varchar2(100) := 'The quick brown fox jumps over the lazy dog.'; --input
 
 BEGIN
-	--get set of all lower case letters(ascii 97-122)
-	for i in ascii('a')..ascii('z')
-	loop
-		c_arr.EXTEND;
-		c_arr(i - ascii('a') + 1):= chr(i); --indices 1-26
-	end loop;
+    
+    for i in ascii('a')..ascii('z')--get set of all lower case letters(ascii 97-122)
+    loop
+        c_arr.EXTEND;
+        c_arr(i - ascii('a') + 1):= chr(i); --indices 1-26
+    end loop;
 
-	--add all characters from input to nested table
-	for i in 1..length(ip)
-	loop
-        	if lower(substr(ip, i, 1)) member of c_arr
-		then
-	        	ip_arr.EXTEND;
-	    		ip_arr(ip_arr.LAST):= lower(substr(ip, i, 1));
-		end if;
-	end loop;
+    
+    for i in 1..length(ip)--add all characters from input to nested table
+    loop
+        if lower(substr(ip, i, 1)) member of c_arr
+        then
+            ip_arr.EXTEND;
+            ip_arr(ip_arr.LAST):= lower(substr(ip, i, 1));
+        end if;
+    end loop;
 
+    for i in 1..c_arr.COUNT --see what letters are missing
+    loop
+        if c_arr(i) not member of ip_arr
+        then
+            missing:= missing || c_arr(i);
+        end if;
+    end loop;
 
-	--see what letters are missing
-	for i in 1..c_arr.COUNT
-	loop
-	        if c_arr(i) not member of ip_arr
-		then
-		        missing:= missing || c_arr(i);
-	        end if;
-	end loop;
-
-	--return if it's a panagram or not
-	if length(missing) is null
-	then
-		dbms_output.put_line('pangram');    
-    	else
-        	dbms_output.put_line('missing ' || missing);
-	end if;
+    --return if it's a panagram or not
+    if length(missing) is null
+    then
+        dbms_output.put_line('pangram');    
+    else
+        dbms_output.put_line('missing ' || missing);
+    end if;
+    
 END;
