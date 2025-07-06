e5f741668c3a9fb30c2b1ce9e852109e809adca7
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-06 15:31:11 -0400

    Update mæting.sql

--- Oracle/PL_SQL/Kattis/mæting/mæting.sql
+++ Oracle/PL_SQL/Kattis/mæting/mæting.sql
@@ -1,20 +1,24 @@
 DECLARE
-	type table_t is table of integer;
-	ip1 table_t := table_t(1,2,3);  --test value from standard input
-	ip2 table_t := table_t(1,2,4); --test value from standard input
-	type ans_t is table of integer index by pls_integer;
-	ans ans_t;
-	idx integer:=1;
+    type table_t is table of integer;
+    ip1 table_t := table_t(1,2,3);  --test value from standard input
+    ip2 table_t := table_t(1,2,4); --test value from standard input
+    type ans_t is table of integer index by pls_integer;
+    ans ans_t;
+    idx integer:=1;
 BEGIN
-    for i in 1..ip1.COUNT loop
-    	if ip1(i) member of ip2 then
-    		ans(idx):=ip1(i);
-		idx:= idx + 1;
-	end if;
+    
+    for i in 1..ip1.COUNT
+    loop
+        if ip1(i) member of ip2
+        then
+            ans(idx):=ip1(i);
+            idx:= idx + 1;
+        end if;
     end loop;
 
-	for i in 1..ans.COUNT loop
-        	dbms_output.put_line(ans(i));
-    	end loop;
+    for i in 1..ans.COUNT
+    loop
+        dbms_output.put_line(ans(i));
+    end loop;
 
 END;
