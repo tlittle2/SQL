34d40669a10afddb5ba8e3329f7b005aafc37791
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-06 15:07:32 -0400

    Update apaxiaaans.sql

--- Oracle/PL_SQL/Kattis/apaxiaaans/apaxiaaans.sql
+++ Oracle/PL_SQL/Kattis/apaxiaaans/apaxiaaans.sql
@@ -1,28 +1,28 @@
 DECLARE
-	type v_arr is table of char(1);
-	arr v_arr := v_arr();
+    type v_arr is table of char(1);
+    arr v_arr := v_arr();
 
-	subtype st_apaxians_word is varchar2(32767);
+    subtype st_apaxians_word is varchar2(32767);
 
-	ip st_apaxians_word := 'rooobert';
-	ans st_apaxians_word;
+    ip st_apaxians_word := 'rooobert';
+    ans st_apaxians_word;
 
 BEGIN
-	for i in 1..length(ip)
-	loop
-		arr.EXTEND;
-		arr(arr.LAST) := substr(ip, i, 1);
-    	end loop;
+    for i in 1..length(ip)
+    loop
+        arr.EXTEND;
+        arr(arr.LAST) := substr(ip, i, 1);
+    end loop;
 
-	for i in 1..arr.COUNT-1
-	LOOP
-		if arr(i) <> arr(i+1)
-		then
-		    ans:= ans || arr(i);
-	    	end if;
-    	END LOOP;
+    for i in 1..arr.COUNT-1
+    LOOP
+        if arr(i) <> arr(i+1)
+        then
+            ans:= ans || arr(i);
+        end if;
+    END LOOP;
 
-	ans:= ans || arr(arr.LAST);
-	dbms_output.put_line(ans);
-
+    ans:= ans || arr(arr.LAST);
+    
+    dbms_output.put_line(ans);
 END;
