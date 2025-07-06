58641e31f9cc08d0126c02b386aa672e2466de30
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-06 15:19:50 -0400

    Update doublepassword.sql

--- Oracle/PL_SQL/Kattis/doublepassword/doublepassword.sql
+++ Oracle/PL_SQL/Kattis/doublepassword/doublepassword.sql
@@ -1,42 +1,43 @@
 DECLARE
-	type arr_t is table of varchar2(4);
-	arr1 arr_t;
-	arr2 arr_t;
+    type arr_t is table of varchar2(4);
+    arr1 arr_t;
+    arr2 arr_t;
 
-	subtype st_iptype is varchar2(4);
+    subtype st_iptype is varchar2(4);
     
-	ip1 st_iptype := '1111';
-	ip2 st_iptype := '1234';
+    ip1 st_iptype := '1111';
+    ip2 st_iptype := '1234';
 
-	procedure populateCollection(p_ipString IN VARCHAR2, p_collection IN OUT arr_t)
-	is
-    	begin
-	        p_collection := arr_t();
-	        for i in 1..length(p_ipString)
-	        loop
-			p_collection.EXTEND;
-			p_collection(i):= substr(p_ipString, i, 1);
-	        end loop;
-    	end;
+    procedure populateCollection(p_ipString IN VARCHAR2, p_collection IN OUT arr_t)
+    is
+    begin
+        p_collection := arr_t();
+        for i in 1..length(p_ipString)
+        loop
+            p_collection.EXTEND;
+            p_collection(i):= substr(p_ipString, i, 1);
+        end loop;
+    end populateCollection;
 
-	function answer(p_collection1 IN arr_t, p_collection2 IN arr_t)
-	return INTEGER
-	IS
-        	c integer:= 0;
-	begin
-		for i in 1..p_collection1.COUNT
-		loop
-		if p_collection1(i) <> p_collection2(i) then
-			c:= c + 1;
-		end if;
-    		end loop;
-		
-		return power(2,c);
-    	end;
+    function answer(p_collection1 IN arr_t, p_collection2 IN arr_t)
+    return INTEGER
+    IS
+        c integer:= 0;
+    begin
+        for i in 1..p_collection1.COUNT
+        loop
+            if p_collection1(i) <> p_collection2(i)
+            then
+                c:= c + 1;
+            end if;
+        end loop;
+        
+        return power(2,c);
+    end answer;
 
 BEGIN
-	populateCollection(ip1, arr1);
-	populateCollection(ip2, arr2);
+    populateCollection(ip1, arr1);
+    populateCollection(ip2, arr2);
 
-	dbms_output.put_line(answer(arr1,arr2));
+    dbms_output.put_line(answer(arr1,arr2));
 END;
