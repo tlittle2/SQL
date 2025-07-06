e7af24af63956f93bb99f51418aa2a20e9baaf42
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-06 15:44:40 -0400

    Update thendaysofchristmas.sql

--- Oracle/PL_SQL/Kattis/thendaysofchristmas/thendaysofchristmas.sql
+++ Oracle/PL_SQL/Kattis/thendaysofchristmas/thendaysofchristmas.sql
@@ -1,49 +1,50 @@
 DECLARE    
-	type t_arr is table of integer index by pls_integer;
-	arr t_arr;
-	
-	ip integer:= 12; --user input
+    type t_arr is table of integer index by pls_integer;
+    arr t_arr;
+    
+    ip integer:= 12; --user input
 
-	PROCEDURE processInput(p_ipString IN INTEGER, p_collection IN OUT t_arr)
-	is
-	begin
-	        for i in 1..p_ipString
-		loop
-	        	p_collection(i):= i;
-	        end loop;
-    	end;
+    PROCEDURE processInput(p_ipString IN INTEGER, p_collection IN OUT t_arr)
+    is
+    begin
+        for i in 1..p_ipString
+        loop
+            p_collection(i):= i;
+        end loop;
+    
+    end processInput;
 
-	FUNCTION calculateOutput1(p_collection IN t_arr)
-	return INTEGER
-	IS
-    		out1 integer:= 0;
-    	begin
-        	for i in p_collection.FIRST..p_collection.LAST
-		loop
-			out1:= out1 + p_collection(i);
-		end loop;
-		
-		return out1;
-    	end;
+    FUNCTION calculateOutput1(p_collection IN t_arr)
+    return INTEGER
+    IS
+        out1 integer:= 0;
+    begin
+        for i in p_collection.FIRST..p_collection.LAST
+        loop
+            out1:= out1 + p_collection(i);
+        end loop;
+        
+        return out1;
+    end calculateOutput1;
 
-	FUNCTION calculateOutput2(p_ipString IN INTEGER)
-	return INTEGER
-	IS
-    		out2 integer:= 0;
-    	begin
-        	for i in 1..p_ipString
-		loop
-	        	for j in 1..i
-			loop
-    				out2:= out2 + j;
-        		end loop;
-    		end loop;
-		
-		return out2;
-    	end;
+    FUNCTION calculateOutput2(p_ipString IN INTEGER)
+    return INTEGER
+    IS
+        out2 integer:= 0;
+    begin
+        for i in 1..p_ipString
+        loop
+            for j in 1..i
+            loop
+                out2:= out2 + j;
+            end loop;
+        end loop;
+        
+       return out2;
+    end calculateOutput2;
 
 BEGIN
-	processInput(ip, arr);
-	dbms_output.put_line(calculateOutput1(arr));
-	dbms_output.put_line(calculateOutput2(ip));
+    processInput(ip, arr);
+    dbms_output.put_line(calculateOutput1(arr));
+    dbms_output.put_line(calculateOutput2(ip));
 END;
