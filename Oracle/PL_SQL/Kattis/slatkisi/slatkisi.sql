57ceb51ae25538551202fa60e311fe90f3eec191
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-06 15:41:36 -0400

    Update slatkisi.sql

--- Oracle/PL_SQL/Kattis/slatkisi/slatkisi.sql
+++ Oracle/PL_SQL/Kattis/slatkisi/slatkisi.sql
@@ -1,15 +1,15 @@
 DECLARE
-	ipString varchar2(12) := '123450995 1';
-  	price number:= to_number(substr(ipString, 1, instr(ipString, ' ')));
-	zeros number:= to_number(substr(ipString, instr(ipString, ' '), length(ipString)));
+    ipString varchar2(12) := '123450995 1';
+    price number:= to_number(substr(ipString, 1, instr(ipString, ' ')));
+    zeros number:= to_number(substr(ipString, instr(ipString, ' '), length(ipString)));
 
-	r varchar2(10) := 1;
+    r varchar2(10) := 1;
 
 BEGIN
-	for i in 1..zeros
-	loop
-    		r := r || '0';
-    	end loop;
-	
-  	dbms_output.put_line(round(price / r) * r);
+    for i in 1..zeros
+    loop
+        r := r || '0';
+    end loop;
+    
+    dbms_output.put_line(round(price / r) * r);
 END;
