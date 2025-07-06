c26d5cc66977c8e66a45a7632e2628a039e88ac2
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-06 15:33:11 -0400

    Update onechicken.sql

--- Oracle/PL_SQL/Kattis/onechicken/onechicken.sql
+++ Oracle/PL_SQL/Kattis/onechicken/onechicken.sql
@@ -1,21 +1,23 @@
 DECLARE
-	ip varchar2(9) := '20 100';
+    ip varchar2(9) := '20 100';
 
-	n integer:= to_number(substr(ip, 1, instr(ip, ' ')-1));
-	m integer:= to_number(substr(ip, instr(ip, ' ')+1, length(ip)));
-			
+    n integer:= to_number(substr(ip, 1, instr(ip, ' ')-1));
+    m integer:= to_number(substr(ip, instr(ip, ' ')+1, length(ip)));
+            
 BEGIN
-	if n < m then
-    		if m - n = 1 then
-    			dbms_output.put_line('Dr. Chaz will have '|| (m-n) || ' piece of chicken left over!');
-		else
-            		dbms_output.put_line('Dr. Chaz will have '|| (m-n) || ' pieces of chicken left over!');
-        	end if;
-	else
-		if n - m = 1 then
-    			dbms_output.put_line('Dr. Chaz needs '|| (n-m) || ' more piece of chicken!');
-		else
-            		dbms_output.put_line('Dr. Chaz needs '|| (n-m) || ' more pieces of chicken!');
-        	end if;
-    	end if;
+    if n < m
+    then
+        if m - n = 1
+        then
+            dbms_output.put_line('Dr. Chaz will have '|| (m-n) || ' piece of chicken left over!');
+        else
+            dbms_output.put_line('Dr. Chaz will have '|| (m-n) || ' pieces of chicken left over!');
+        end if;
+    else
+        if n - m = 1 then
+            dbms_output.put_line('Dr. Chaz needs '|| (n-m) || ' more piece of chicken!');
+        else
+            dbms_output.put_line('Dr. Chaz needs '|| (n-m) || ' more pieces of chicken!');
+        end if;
+    end if;
 END;
