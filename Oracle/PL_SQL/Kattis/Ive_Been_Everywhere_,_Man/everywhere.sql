66f0f4190ddca3143346dfe6fe28f12947891ab3
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-06 14:42:40 -0400

    Update everywhere.sql

--- Oracle/PL_SQL/Kattis/Ive_Been_Everywhere_,_Man/everywhere.sql
+++ Oracle/PL_SQL/Kattis/Ive_Been_Everywhere_,_Man/everywhere.sql
@@ -1,12 +1,11 @@
 DECLARE
-	type nest_t is table of varchar2(100);
-	cities nest_t := nest_t('edmonton','edmonton','edmonton');
-	city_set nest_t:= set(cities); --this allows to you get distinct values from nested tables
+    type nest_t is table of varchar2(100);
+    cities nest_t := nest_t('edmonton','edmonton','edmonton');
+    city_set nest_t:= set(cities); --this allows to you get distinct values from nested tables
 
 BEGIN
-	for i in 1..city_set.COUNT
-	loop
-    		dbms_output.put_line(city_set(i));
-    	end loop;
-
+    for i in 1..city_set.COUNT
+    loop
+        dbms_output.put_line(city_set(i));
+    end loop;
 END;
