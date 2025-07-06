38cd809be143a67b9db3bbfb62b892b8041b81ae
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-06 15:40:05 -0400

    Update simonsays.sql

--- Oracle/PL_SQL/Kattis/simonsays/simonsays.sql
+++ Oracle/PL_SQL/Kattis/simonsays/simonsays.sql
@@ -1,14 +1,14 @@
 DECLARE
-	type v_arr is table of varchar2(100);
-	arr v_arr := v_arr('Raise your right hand.','Lower your right hand.','Simon says raise your left hand.');
-	saying char(10) := 'Simon says';
+    type v_arr is table of varchar2(100);
+    arr v_arr := v_arr('Raise your right hand.','Lower your right hand.','Simon says raise your left hand.');
+    saying char(10) := 'Simon says';
 
 BEGIN
-	for i in 1..arr.COUNT
-	loop
-		if instr(arr(i), saying) > 0
-		then
-			dbms_output.put_line(substr(arr(i), instr(arr(i) , saying) + length(saying), length(arr(i))));
-    		end if;
-    	end loop;
+    for i in 1..arr.COUNT
+    loop
+        if instr(arr(i), saying) > 0
+        then
+            dbms_output.put_line(substr(arr(i), instr(arr(i) , saying) + length(saying), length(arr(i))));
+        end if;
+    end loop;
 END;
