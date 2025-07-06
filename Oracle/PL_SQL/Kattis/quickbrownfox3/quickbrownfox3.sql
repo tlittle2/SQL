c8b4440f8046a2f62cae47b03658134b33c68460
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-06 15:38:59 -0400

    Update quickbrownfox3.sql

--- Oracle/PL_SQL/Kattis/quickbrownfox3/quickbrownfox3.sql
+++ Oracle/PL_SQL/Kattis/quickbrownfox3/quickbrownfox3.sql
@@ -1,60 +1,62 @@
 DECLARE
-	type char_arr_t is table of char(1);
-	c_arr char_arr_t:= char_arr_t();
-	ip_arr char_arr_t:= char_arr_t();
-	ip varchar2(100) := 'the quick brown foxes jumped over the lazy dog';
+    type char_arr_t is table of char(1);
+    c_arr char_arr_t:= char_arr_t();
+    ip_arr char_arr_t:= char_arr_t();
+    ip varchar2(100) := 'the quick brown foxes jumped over the lazy dog';
 
-	procedure getSetOfCharacters(p_chrset IN OUT char_arr_t)
-	is
-	begin
-		for i in ascii('a')..ascii('z') --97-122
-		loop
-	    		p_chrset.EXTEND;
-			p_chrset(i - ascii('a') + 1):= chr(i); --indices 1-26
-		end loop;
-	end;
-	
-	procedure processInput(p_ipString IN VARCHAR2, p_c_arr IN char_arr_t, p_ip_arr IN OUT char_arr_t)
-	is 
-	begin
-		for i in 1..length(p_ipString)
-		loop
-	        	if lower(substr(p_ipString, i, 1)) member of p_c_arr then
-		        	p_ip_arr.EXTEND;
-		    		p_ip_arr(p_ip_arr.LAST):= lower(substr(p_ipString, i, 1));
-			end if;
-		end loop;
-	end;
+    procedure getSetOfCharacters(p_chrset IN OUT char_arr_t)
+    is
+    begin
+        for i in ascii('a')..ascii('z') --97-122
+        loop
+            p_chrset.EXTEND;
+            p_chrset(i - ascii('a') + 1):= chr(i); --indices 1-26
+        end loop;
+    end getSetOfCharacters;
+    
+    procedure processInput(p_ipString IN VARCHAR2, p_c_arr IN char_arr_t, p_ip_arr IN OUT char_arr_t)
+    is 
+    begin
+        for i in 1..length(p_ipString)
+        loop
+            if lower(substr(p_ipString, i, 1)) member of p_c_arr
+            then
+                p_ip_arr.EXTEND;
+                p_ip_arr(p_ip_arr.LAST):= lower(substr(p_ipString, i, 1));
+            end if;
+        end loop;
+    end processInput;
 
-	function computeMissing(p_c_arr IN char_arr_t, p_ip_arr IN char_arr_t)
-	return varchar2
-	is
-		p_outString varchar2(26);
-	begin
-		for i in 1..p_c_arr.COUNT
-		loop
-		        if p_c_arr(i) not member of p_ip_arr
-			then
-		        	p_outString:= p_outString || p_c_arr(i);
-		        end if;
-		end loop;
-		return p_outString;
-	end;
-	
-	function isPanagram(p_ip_string VARCHAR2)
-	RETURN VARCHAR2
-	IS
-	begin
-		if length(p_ip_string) is null
-		then
-			RETURN 'pangram';
-	    	else
-	        	RETURN 'missing ' || p_ip_string;
-		end if;
-	end;
+    function computeMissing(p_c_arr IN char_arr_t, p_ip_arr IN char_arr_t)
+    return varchar2
+    is
+        p_outString varchar2(26);
+    begin
+        for i in 1..p_c_arr.COUNT
+        loop
+            if p_c_arr(i) not member of p_ip_arr
+            then
+                p_outString:= p_outString || p_c_arr(i);
+            end if;
+        end loop;
+        
+        return p_outString;
+    end computeMissing;
+    
+    function isPanagram(p_ip_string VARCHAR2)
+    RETURN VARCHAR2
+    IS
+    begin
+        if length(p_ip_string) is null
+        then
+            RETURN 'pangram';
+        else
+            RETURN 'missing ' || p_ip_string;
+        end if;
+    end isPanagram;
 
 BEGIN
-  	getSetOfCharacters(c_arr);
-	processInput(ip,c_arr,ip_arr);
-	dbms_output.put_line(isPanagram(computeMissing(c_arr, ip_arr)));
+    getSetOfCharacters(c_arr);
+    processInput(ip,c_arr,ip_arr);
+    dbms_output.put_line(isPanagram(computeMissing(c_arr, ip_arr)));
 END;
