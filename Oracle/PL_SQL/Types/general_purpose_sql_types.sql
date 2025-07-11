8ffff9589d21ca4697e77083cc30f9aea4ad9002
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-11 19:09:38 -0400

    Update general_purpose_sql_types.sql

--- Oracle/PL_SQL/Types/general_purpose_sql_types.sql
+++ Oracle/PL_SQL/Types/general_purpose_sql_types.sql
@@ -1,4 +1,4 @@
-create type t_str_array as table of varchar2(4000); --whatever the max length allowed for DDL is
+create type t_str_array as table of varchar2(4000); --whatever the max length allowed for tables is
 /
 
 create type t_date_array as table of date;
