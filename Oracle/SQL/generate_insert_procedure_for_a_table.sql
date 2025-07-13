b70066d814efb1f8e147df5efc97e6395de8e763
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-12 21:57:45 -0400

    Update generate_insert_procedure_for_a_table.sql

--- Oracle/SQL/generate_insert_procedure_for_a_table.sql
+++ Oracle/SQL/generate_insert_procedure_for_a_table.sql
@@ -1,7 +1,8 @@
 select lower(stmnt) from (
 select 'procedure insert_' || table_name || '(' || LISTAGG('p_' || COLUMN_NAME || ' ' || column_name || '%type DEFAULT NULL', ' , ') WITHIN GROUP (ORDER BY COLUMN_ID) || ')'
 || 'is begin INSERT INTO '
-|| table_name || ' VALUES (' || LISTAGG('p_' || COLUMN_NAME, ' , ')  WITHIN GROUP (ORDER BY COLUMN_ID)
+|| table_name
+|| ' VALUES (' || LISTAGG('p_' || COLUMN_NAME, ' , ')  WITHIN GROUP (ORDER BY COLUMN_ID)
 || '); end insert_' || table_name || ';' as stmnt
 from user_tab_columns
 where table_name = 'ASTROLOGY'
