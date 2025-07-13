d35862403fa01fb7869f20de03fc920df74af559
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-12 21:47:08 -0400

    Update generate_insert_procedure.sql

--- Oracle/SQL/generate_insert_procedure.sql
+++ Oracle/SQL/generate_insert_procedure.sql
@@ -1,8 +1,8 @@
-select REGEXP_REPLACE(stmnt, ',([^,]*)$', ')\1') AS stmnt_fixed from (
+select lower(stmnt) from (
 select 'procedure insert_' || lower(table_name) || '(' || LISTAGG('p_' || COLUMN_NAME || ' ' || column_name || '%type DEFAULT NULL', ' , ') WITHIN GROUP (ORDER BY COLUMN_ID) || ')'
 || 'is begin INSERT INTO '
-|| lower(table_name) || ' VALUES (' || LISTAGG('p_' || COLUMN_NAME || ' , ')  WITHIN GROUP (ORDER BY COLUMN_ID)
-|| '; end;' as stmnt
+|| lower(table_name) || ' VALUES (' || LISTAGG('p_' || COLUMN_NAME, ' , ')  WITHIN GROUP (ORDER BY COLUMN_ID)
+|| '); end;' as stmnt
 from user_tab_columns
 where table_name = 'ASTROLOGY'
 GROUP BY TABLE_NAME
