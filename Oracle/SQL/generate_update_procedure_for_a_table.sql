2e4f477e4cd27bb01b32cb8e9fb21b11e9f5a84d
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-12 21:40:12 -0400

    Update generate_update_procedure_for_a_table.sql

--- Oracle/SQL/generate_update_procedure_for_a_table.sql
+++ Oracle/SQL/generate_update_procedure_for_a_table.sql
@@ -1,6 +1,8 @@
-select 'procedure update_' || lower(table_name) || '(' || LISTAGG('p_' || COLUMN_NAME || ' ' || column_name || '%type', ' , ') WITHIN GROUP (ORDER BY COLUMN_ID) || ')'
-|| 'is begin UPDATE ' || lower(table_name) || ' set ' || LISTAGG(COLUMN_NAME || ' = nvl(' ||'p_' || COLUMN_NAME || ',' || column_name || ')', ' , ') WITHIN GROUP (ORDER BY COLUMN_ID)
-|| '; end;'
-from all_tab_columns
-where table_name = 'SALARY_DATA_STG'
-GROUP BY TABLE_NAME;
+select lower(stmnt) from (
+select 'procedure update_' || tab.table_name || '(' || LISTAGG('p_' || tab.COLUMN_NAME || ' ' || tab.table_name || '.' || tab.column_name || '%type', ' , ') WITHIN GROUP (ORDER BY tab.COLUMN_ID) || ')'
+|| 'is begin UPDATE ' || tab.table_name || ' set ' || LISTAGG(tab.COLUMN_NAME || ' = nvl(' ||'p_' || tab.COLUMN_NAME || ',' || tab.column_name || ')', ' , ') WITHIN GROUP (ORDER BY tab.COLUMN_ID)
+|| '; end;' as stmnt
+from user_tab_columns tab
+where tab.table_name = 'SALARY_DATA_STG'
+GROUP BY tab.TABLE_NAME
+);
