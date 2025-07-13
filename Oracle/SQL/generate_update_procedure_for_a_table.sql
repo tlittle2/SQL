883693daf79a2a7e26a7f07b41988fe3e93c27d6
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-12 21:59:07 -0400

    Update generate_update_procedure_for_a_table.sql

--- Oracle/SQL/generate_update_procedure_for_a_table.sql
+++ Oracle/SQL/generate_update_procedure_for_a_table.sql
@@ -1,5 +1,6 @@
 select lower(stmnt) from (
-select 'procedure update_' || tab.table_name || '(' || LISTAGG('p_' || tab.COLUMN_NAME || ' ' || tab.table_name || '.' || tab.column_name || '%type DEFAULT NULL', ' , ') WITHIN GROUP (ORDER BY tab.COLUMN_ID) || ')'
+select 'procedure update_' || tab.table_name
+|| '(' || LISTAGG('p_' || tab.COLUMN_NAME || ' ' || tab.table_name || '.' || tab.column_name || '%type DEFAULT NULL', ' , ') WITHIN GROUP (ORDER BY tab.COLUMN_ID) || ')'
 || 'is begin UPDATE '
 || tab.table_name || ' set ' || LISTAGG(tab.COLUMN_NAME || ' = nvl(' ||'p_' || tab.COLUMN_NAME || ',' || tab.column_name || ')', ' , ') WITHIN GROUP (ORDER BY tab.COLUMN_ID)
 || '; end update_' || tab.table_name || ';' as stmnt
