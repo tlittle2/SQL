296320425820b297ea951caac96cfa6782e57885
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-25 19:48:40 -0400

    Update generate_select_into_rowtype_procedure_for_a_table.sql

--- Oracle/SQL/generate_select_into_rowtype_procedure_for_a_table.sql
+++ Oracle/SQL/generate_select_into_rowtype_procedure_for_a_table.sql
@@ -1,14 +1,15 @@
 select lower(stmnt) from (
-select 'procedure get_' || columns.table_name || '_1(' || LISTAGG('p_' || columns.COLUMN_NAME || ' IN ' || columns.table_name || '.' || columns.column_name || '%type', ',') WITHIN GROUP (ORDER BY columns.position)
+select 'procedure get_' || columns.table_name || '_1' -- change number here if you want multiple "getter" procedures
+|| '(' || LISTAGG('p_' || columns.COLUMN_NAME || ' IN ' || columns.table_name || '.' || columns.column_name || '%type', ',') WITHIN GROUP (ORDER BY columns.position)
 || ',p_' || columns.table_name || '_row IN OUT ' || columns.table_name || '%rowtype)'
 || 'is begin select * into  p_' || columns.table_name || '_row' || ' from ' || columns.table_name || ' WHERE ' || LISTAGG(columns.COLUMN_NAME || '=' || 'p_' || columns.COLUMN_NAME, ' and ') WITHIN GROUP (ORDER BY columns.position)
-|| '; end get_' || columns.table_name || '_1;'
+|| '; end get_' || columns.table_name || '_1;'       -- change number here if you want multiple "getter" procedures
 as stmnt
 from user_constraints constraints
 inner join user_cons_columns columns
 on constraints.table_name  = columns.table_name
 and constraints.constraint_name = columns.constraint_name
-where constraints.table_name = 'ARCHIVE_RULES'
-and constraints.constraint_type = 'P' --for columns that are the primary key (most practical)
+where constraints.table_name = 'SALARY_DATA_STG'
+and constraints.constraint_type = 'P'
 GROUP BY columns.TABLE_NAME
 );
