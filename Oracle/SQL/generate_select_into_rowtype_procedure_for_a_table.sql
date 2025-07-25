b0f08cb7b19049d4106a44a0c458f3bf1df8b780
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-25 19:50:30 -0400

    Update generate_select_into_rowtype_procedure_for_a_table.sql

--- Oracle/SQL/generate_select_into_rowtype_procedure_for_a_table.sql
+++ Oracle/SQL/generate_select_into_rowtype_procedure_for_a_table.sql
@@ -1,3 +1,4 @@
+--for primary key columns
 select lower(stmnt) from (
 select 'procedure get_' || columns.table_name || '_1' -- change number here if you want multiple "getter" procedures
 || '(' || LISTAGG('p_' || columns.COLUMN_NAME || ' IN ' || columns.table_name || '.' || columns.column_name || '%type', ',') WITHIN GROUP (ORDER BY columns.position)
@@ -11,5 +12,20 @@ on constraints.table_name  = columns.table_name
 and constraints.constraint_name = columns.constraint_name
 where constraints.table_name = 'SALARY_DATA_STG'
 and constraints.constraint_type = 'P'
+GROUP BY columns.TABLE_NAME
+);
+
+
+--for custom columns
+select lower(stmnt) from (
+select 'procedure get_' || columns.table_name || '_1'
+|| '(' || LISTAGG('p_' || columns.COLUMN_NAME || ' IN ' || columns.table_name || '.' || columns.column_name || '%type', ',') WITHIN GROUP (ORDER BY columns.column_id)
+|| ',p_' || columns.table_name || '_row IN OUT ' || columns.table_name || '%rowtype)'
+|| 'is begin select * into  p_' || columns.table_name || '_row' || ' from ' || columns.table_name || ' WHERE ' || LISTAGG(columns.COLUMN_NAME || '=' || 'p_' || columns.COLUMN_NAME, ' and ') WITHIN GROUP (ORDER BY columns.column_id)
+|| '; end get_' || columns.table_name || '_1;'
+as stmnt
+from user_tab_columns columns
+where columns.table_name = 'SALARY_DATA_STG'
+and column_name in ('CASE_NUM', 'ID')
 GROUP BY columns.TABLE_NAME
 );
