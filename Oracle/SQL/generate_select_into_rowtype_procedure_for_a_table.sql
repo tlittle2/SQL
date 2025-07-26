16977a1ffa6bb0a47a7872fd3aa5dd09cd91a70e
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-26 11:23:14 -0400

    Update generate_select_into_rowtype_procedure_for_a_table.sql

--- Oracle/SQL/generate_select_into_rowtype_procedure_for_a_table.sql
+++ Oracle/SQL/generate_select_into_rowtype_procedure_for_a_table.sql
@@ -6,7 +6,7 @@ select 'procedure get_' || columns.table_name || '_1' -- change number here if y
 || ',p_' || columns.table_name || '_row IN OUT ' || columns.table_name || '%rowtype'
 || ')'
 || 'is begin select * into  p_' || columns.table_name || '_row' || ' from ' || columns.table_name || ' WHERE ' || LISTAGG(columns.COLUMN_NAME || '=' || 'p_' || columns.COLUMN_NAME, ' and ') WITHIN GROUP (ORDER BY columns.position)
-|| '; exception when no_data_found then raise; when too_many_rows then raise; end get_' || columns.table_name || '_1;' -- change number here if you want multiple "getter" procedures
+|| '; exception when no_data_found then raise; when too_many_rows then raise; when others then raise; end get_' || columns.table_name || '_1;' -- change number here if you want multiple "getter" procedures
 as stmnt
 from user_constraints constraints
 inner join user_cons_columns columns
@@ -17,6 +17,7 @@ and constraints.constraint_type = 'P'
 GROUP BY columns.TABLE_NAME
 );
 
+
 --for indexed columns
 select lower(stmnt) from (
 select 'procedure get_' || columns.table_name || '_2' -- change number here if you want multiple "getter" procedures
@@ -25,7 +26,7 @@ select 'procedure get_' || columns.table_name || '_2' -- change number here if y
 || LISTAGG('p_' || columns.COLUMN_NAME || ' IN ' || columns.table_name || '.' || columns.column_name || '%type', ',') WITHIN GROUP (ORDER BY columns.COLUMN_POSITION)
 || ')'
 || 'is begin select * into  p_' || columns.table_name || '_row' || ' from ' || columns.table_name || ' WHERE ' || LISTAGG(columns.COLUMN_NAME || '=' || 'p_' || columns.COLUMN_NAME, ' and ') WITHIN GROUP (ORDER BY columns.COLUMN_POSITION)
-|| '; exception when no_data_found then raise; when too_many_rows then raise; end get_' || columns.table_name || '_2;' -- change number here if you want multiple "getter" procedures
+|| '; exception when no_data_found then raise; when too_many_rows then raise; when others then raise; end get_' || columns.table_name || '_2;' -- change number here if you want multiple "getter" procedures
 as stmnt
 from user_ind_columns columns
 where columns.table_name in('DIM_APP_TABLES', 'UPDATE_MATCH')
@@ -41,7 +42,7 @@ select 'procedure get_' || columns.table_name || '_1' -- change number here if y
 || ',p_' || columns.table_name || '_row IN OUT ' || columns.table_name || '%rowtype'
 || ')'
 || 'is begin select * into  p_' || columns.table_name || '_row' || ' from ' || columns.table_name || ' WHERE ' || LISTAGG(columns.COLUMN_NAME || '=' || 'p_' || columns.COLUMN_NAME, ' and ') WITHIN GROUP (ORDER BY columns.column_id)
-|| '; exception when no_data_found then raise; when too_many_rows then raise; end get_' || columns.table_name || '_1;' -- change number here if you want multiple "getter" procedures
+|| '; exception when no_data_found then raise; when too_many_rows then raise; when others then raise; end get_' || columns.table_name || '_1;' -- change number here if you want multiple "getter" procedures
 as stmnt
 from user_tab_columns columns
 where columns.table_name = 'SALARY_DATA_STG'
