637c03d2fba4cd4105284ccccd04866bd848a500
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-26 18:11:45 -0400

    Update part_parm_pf_check.sql

--- Oracle/PL_SQL/Triggers/part_parm_pf_check.sql
+++ Oracle/PL_SQL/Triggers/part_parm_pf_check.sql
@@ -4,7 +4,7 @@ for each row
 begin
     if not archive_rules_tbl_pkg.is_correct_arch_prefix(:new.table_name)
     then
-        assert_pkg.is_true(1 = 2, 'ABORTING! TRYING TO INSERT ARCHIVE TABLE WITHOUT PROPER PREFIX');
+        assert_pkg.is_true(1 = 2, 'ABORTING! TRYING TO INSERT ARCHIVE TABLE WITHOUT PROPER PREFIX');--do not insert on purpose we are trying to insert an archive table with the wrong prefix
     end if;
 
 end part_parm_pf_check;
