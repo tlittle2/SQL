6b6fd7bd9b9cf8613fb4e23a51dbf7f04bc16919
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-26 18:06:48 -0400

    Update arch_rules_pf_check.sql

--- Oracle/PL_SQL/Triggers/arch_rules_pf_check.sql
+++ Oracle/PL_SQL/Triggers/arch_rules_pf_check.sql
@@ -1,7 +1,6 @@
 create or replace trigger arch_rules_pf_check
 before insert or update on archive_rules
 for each row
-declare
 begin
     if not archive_rules_tbl_pkg.is_correct_arch_prefix(:new.table_name)
     then
