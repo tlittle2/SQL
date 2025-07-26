c74739214b503415f2af23c3a90a20217a591eae
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-26 18:23:30 -0400

    Update and rename part_parm_pf_check.sql to part_parm_pf_check_trg.sql

--- Oracle/PL_SQL/Triggers/part_parm_pf_check_trg.sql
+++ Oracle/PL_SQL/Triggers/part_parm_pf_check_trg.sql
@@ -1,4 +1,4 @@
-create or replace trigger part_parm_pf_check
+create or replace trigger part_parm_pf_check_trg
 before insert or update on partition_table_parm
 for each row
 begin
@@ -7,4 +7,4 @@ begin
         assert_pkg.is_true(1 = 2, 'ABORTING! TRYING TO INSERT ARCHIVE TABLE WITHOUT PROPER PREFIX');--do not insert on purpose we are trying to insert an archive table with the wrong prefix
     end if;
 
-end part_parm_pf_check;
+end part_parm_pf_check_trg;
