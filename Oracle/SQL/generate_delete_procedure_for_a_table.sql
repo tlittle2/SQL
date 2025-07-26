b412409cdb5ef27eeb4ccf021791354a7c994a36
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-25 20:23:30 -0400

    Update generate_delete_procedure_for_a_table.sql

--- Oracle/SQL/generate_delete_procedure_for_a_table.sql
+++ Oracle/SQL/generate_delete_procedure_for_a_table.sql
@@ -1,3 +1,4 @@
+--primary key columns
 select lower(stmnt) from (
 select 'procedure delete_' || columns.table_name || '(' || LISTAGG('p_' || columns.COLUMN_NAME || ' ' || columns.table_name || '.' || columns.column_name || '%type', ' , ') WITHIN GROUP (ORDER BY columns.position) || ')'
 || 'is begin DELETE FROM ' || columns.table_name || ' WHERE ' || LISTAGG(columns.COLUMN_NAME || '=' || 'p_' || columns.COLUMN_NAME, ' and ') WITHIN GROUP (ORDER BY columns.position)
@@ -9,5 +10,18 @@ on constraints.table_name  = columns.table_name
 and constraints.constraint_name = columns.constraint_name
 where constraints.table_name = 'ARCHIVE_RULES'
 and constraints.constraint_type = 'P'
+GROUP BY columns.TABLE_NAME
+);
+
+
+--custom columns
+select lower(stmnt) from (
+select 'procedure delete_' || columns.table_name || '(' || LISTAGG('p_' || columns.COLUMN_NAME || ' ' || columns.table_name || '.' || columns.column_name || '%type', ' , ') WITHIN GROUP (ORDER BY columns.column_Id) || ')'
+|| 'is begin DELETE FROM ' || columns.table_name || ' WHERE ' || LISTAGG(columns.COLUMN_NAME || '=' || 'p_' || columns.COLUMN_NAME, ' and ') WITHIN GROUP (ORDER BY columns.column_Id)
+|| '; end delete_' || columns.table_name || ';'
+as stmnt
+from user_tab_columns columns
+where columns.table_name = 'ARCHIVE_RULES'
+and columns.column_name = 'TABLE_OWNER'
 GROUP BY columns.TABLE_NAME
 );
