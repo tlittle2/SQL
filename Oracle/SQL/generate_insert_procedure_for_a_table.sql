524c9cbbcb9a1b09e3ff5b2194d07ae3f5d7c2b4
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-26 10:40:21 -0400

    Update generate_insert_procedure_for_a_table.sql

--- Oracle/SQL/generate_insert_procedure_for_a_table.sql
+++ Oracle/SQL/generate_insert_procedure_for_a_table.sql
@@ -7,6 +7,6 @@ select 'procedure insert_' || table_name || '(' || LISTAGG('p_' || COLUMN_NAME |
 || LISTAGG('p_' || COLUMN_NAME, ' , ')  WITHIN GROUP (ORDER BY COLUMN_ID)
 || '); end insert_' || table_name || ';' as stmnt
 from user_tab_columns
-where table_name = 'ASTROLOGY'
+where table_name = 'ASTROLOGY' --modify procedure accordingly for your particular where clause
 GROUP BY TABLE_NAME
-); --modify procedure accordingly for your particular where clause
+);
