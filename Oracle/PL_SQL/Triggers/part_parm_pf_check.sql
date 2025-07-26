535b1da88916ea58b90723a327844bf5e875eadb
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-26 18:06:23 -0400

    Update part_parm_pf_check.sql

--- Oracle/PL_SQL/Triggers/part_parm_pf_check.sql
+++ Oracle/PL_SQL/Triggers/part_parm_pf_check.sql
@@ -1,7 +1,6 @@
 create or replace trigger part_parm_pf_check
 before insert or update on partition_table_parm
 for each row
-declare
 begin
     if not archive_rules_tbl_pkg.is_correct_arch_prefix(:new.table_name)
     then
