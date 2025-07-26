a8e012b58627f29acf373d17773438711cd1d907
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-26 09:58:42 -0400

    Update generate_update_procedure_for_a_table.sql

--- Oracle/SQL/generate_update_procedure_for_a_table.sql
+++ Oracle/SQL/generate_update_procedure_for_a_table.sql
@@ -11,22 +11,34 @@ GROUP BY tab.TABLE_NAME
 );
 
 
---Kinda close for adding a where claused based on constraints on a table (could be used for primary key constraints, indexes, partitions)
-  --Need to have some way for not allowing the WHERE clause to not be DEFAULT NULL (and maybe even be the starting parameters to the procedure)
-select lower(stmnt) from (
-select 'procedure update_' || tab.table_name
-|| '_2(' || LISTAGG('p_' || tab.COLUMN_NAME || ' ' || tab.table_name || '.' || tab.column_name || '%type DEFAULT NULL', ' , ') WITHIN GROUP (ORDER BY tab.COLUMN_ID) || ')'
-|| 'is begin UPDATE '
-|| tab.table_name || ' set ' || LISTAGG(tab.COLUMN_NAME || ' = nvl(' ||'p_' || tab.COLUMN_NAME || ',' || tab.column_name || ')', ' , ') WITHIN GROUP (ORDER BY tab.COLUMN_ID)
-|| ' WHERE ' || columns.column_name || '= p_' || columns.column_name
-|| '; end update_' || tab.table_name || ';' as stmnt
-from user_tab_columns tab
-inner join user_constraints constraints
-on tab.table_name  = constraints.table_name
-inner join user_cons_columns columns
-on constraints.table_name  = columns.table_name
-and constraints.constraint_name = columns.constraint_name
-where constraints.constraint_type = 'P'
-and tab.table_name in ('SALARY_DATA_STG', 'ASTROLOGY', 'CONTROL_REPS')
-GROUP BY tab.TABLE_NAME, columns.column_name
+--where clause (need to find away to isolate PK constraints only for this particular query)
+with ds as (
+SELECT
+    tab.table_name   tbl
+  , tab.column_name  AS tab_column
+  , cons.column_name AS cons_column
+  , coalesce(cons.column_name , tab.column_name) as all_cols
+  , tab.column_id
+from 
+user_tab_columns tab
+left outer join user_cons_columns cons
+on tab.table_name = cons.table_name
+and tab.column_name = cons.column_name
+where tab.table_name = 'SALARY_DATA_STG'
+order by case when cons.column_name is not null then 0 else 1 end asc nulls first, column_id
+)
+
+select lower(stmnt) as stmnt from (
+select
+'procedure update_' || tbl
+|| '_2('
+|| LISTAGG('p_' || case when tab_column = cons_column then cons_column || ' ' || tbl || '.' || cons_column || '%type' else tab_column || ' ' || tbl || '.' || tab_column || '%type DEFAULT NULL' end, ' , ') WITHIN GROUP (ORDER BY column_id) || ')'
+|| 'is begin'
+|| ' UPDATE '
+|| tbl || ' set ' || (select LISTAGG(tab_column || ' = nvl(' ||'p_' || tab_column || ',' || tab_column || ')' , ' , ') WITHIN GROUP (ORDER BY column_id) from ds where cons_column is null)
+|| ' where ' || (select LISTAGG(cons_column || ' = p_' || cons_column , ' and ') WITHIN GROUP (ORDER BY column_id) from ds where cons_column is not null)
+|| '; end update_' || tbl || ';' as stmnt
+from ds ds
+group by tbl
 );
+
