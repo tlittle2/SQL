2559dee027ce0c8e8211f064f0a7daa51c198f19
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-26 11:20:24 -0400

    Update generate_delete_procedure_for_a_table.sql

--- Oracle/SQL/generate_delete_procedure_for_a_table.sql
+++ Oracle/SQL/generate_delete_procedure_for_a_table.sql
@@ -2,7 +2,7 @@
 select lower(stmnt) from (
 select 'procedure delete_' || columns.table_name || '_2(' || LISTAGG('p_' || columns.COLUMN_NAME || ' ' || columns.table_name || '.' || columns.column_name || '%type', ' , ') WITHIN GROUP (ORDER BY columns.position) || ')'
 || 'is begin DELETE FROM ' || columns.table_name || ' WHERE ' || LISTAGG(columns.COLUMN_NAME || '=' || 'p_' || columns.COLUMN_NAME, ' and ') WITHIN GROUP (ORDER BY columns.position)
-|| '; end delete_' || columns.table_name || ';'
+|| '; exception when others then raise; end delete_' || columns.table_name || ';'
 as stmnt
 from user_constraints constraints
 inner join user_cons_columns columns
@@ -17,7 +17,7 @@ GROUP BY columns.TABLE_NAME
 select lower(stmnt) from (
 select 'procedure delete_' || columns.table_name || '_3(' || LISTAGG('p_' || columns.COLUMN_NAME || ' ' || columns.table_name || '.' || columns.column_name || '%type', ' , ') WITHIN GROUP (ORDER BY columns.COLUMN_POSITION) || ')'
 || 'is begin DELETE FROM ' || columns.table_name || ' WHERE ' || LISTAGG(columns.COLUMN_NAME || '=' || 'p_' || columns.COLUMN_NAME, ' and ') WITHIN GROUP (ORDER BY columns.COLUMN_POSITION)
-|| '; end delete_' || columns.table_name || ';'
+|| '; exception when others then raise; end delete_' || columns.table_name || ';'
 as stmnt
 from user_ind_columns columns
 where columns.table_name = 'ARCHIVE_RULES'
@@ -29,7 +29,7 @@ GROUP BY columns.TABLE_NAME
 select lower(stmnt) from (
 select 'procedure delete_' || columns.table_name || '(' || LISTAGG('p_' || columns.COLUMN_NAME || ' ' || columns.table_name || '.' || columns.column_name || '%type', ' , ') WITHIN GROUP (ORDER BY columns.column_Id) || ')'
 || 'is begin DELETE FROM ' || columns.table_name || ' WHERE ' || LISTAGG(columns.COLUMN_NAME || '=' || 'p_' || columns.COLUMN_NAME, ' and ') WITHIN GROUP (ORDER BY columns.column_Id)
-|| '; end delete_' || columns.table_name || ';'
+|| '; exception when others then raise; end delete_' || columns.table_name || ';'
 as stmnt
 from user_tab_columns columns
 where columns.table_name = 'ARCHIVE_RULES'
