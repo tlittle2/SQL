b7c1f18901c6cc0755b015bb8cbbeef470e56237
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-25 20:21:37 -0400

    Update generate_select_into_rowtype_procedure_for_a_table.sql

--- Oracle/SQL/generate_select_into_rowtype_procedure_for_a_table.sql
+++ Oracle/SQL/generate_select_into_rowtype_procedure_for_a_table.sql
@@ -17,6 +17,21 @@ and constraints.constraint_type = 'P'
 GROUP BY columns.TABLE_NAME
 );
 
+--for indexed columns
+select lower(stmnt) from (
+select 'procedure get_' || columns.table_name || '_2' -- change number here if you want multiple "getter" procedures
+|| '('
+|| 'p_' || columns.table_name || '_row IN OUT ' || columns.table_name || '%rowtype,'
+|| LISTAGG('p_' || columns.COLUMN_NAME || ' IN ' || columns.table_name || '.' || columns.column_name || '%type', ',') WITHIN GROUP (ORDER BY columns.COLUMN_POSITION)
+|| ')'
+|| 'is begin select * into  p_' || columns.table_name || '_row' || ' from ' || columns.table_name || ' WHERE ' || LISTAGG(columns.COLUMN_NAME || '=' || 'p_' || columns.COLUMN_NAME, ' and ') WITHIN GROUP (ORDER BY columns.COLUMN_POSITION)
+|| '; exception when no_data_found then raise; when too_many_rows then raise; end get_' || columns.table_name || '_2;' -- change number here if you want multiple "getter" procedures
+as stmnt
+from user_ind_columns columns
+where columns.table_name in('DIM_APP_TABLES', 'UPDATE_MATCH')
+GROUP BY columns.TABLE_NAME
+);
+
 
 --for custom columns
 select lower(stmnt) from (
