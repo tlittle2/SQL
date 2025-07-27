6dde1db6c96caf5901dbb12a4c16bfdfcddb110c
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-27 10:18:33 -0400

    Update oracle_sql_to_java_object.sql

--- Oracle/SQL/oracle_sql_to_java_object.sql
+++ Oracle/SQL/oracle_sql_to_java_object.sql
@@ -12,12 +12,14 @@ select 'private ' || case
     when upper(data_type) = 'NUMBER' and nvl(data_precision,0) > 0 then 'double'
     when upper(data_type) = 'INTEGER' then 'int'
     when upper(data_type) = 'DATE' then 'java.util.Date'
-    when upper(data_type) = 'TIMESTAMP' then 'java.util.Date'
+    when upper(substr(data_type, 1,9)) = 'TIMESTAMP' then 'java.util.Date'
     when upper(data_type) = 'FLOAT' then 'float'
     when upper(data_type) = 'VARCHAR2' then 'String'
     when upper(data_type) = 'NVARCHAR2' then 'String'
     end || ' ' || replace(converted, substr(converted,1,1), lower(substr(converted,1,1))) || ';' as java_type 
  from ds;
+
+
 
 
 --original
@@ -28,7 +30,7 @@ select column_name, data_type, data_length, data_precision
     when upper(data_type) = 'NUMBER' and nvl(data_precision,0) > 0 then 'double'
     when upper(data_type) = 'INTEGER' then 'int'
     when upper(data_type) = 'DATE' then 'java.util.Date'
-    when upper(data_type) = 'TIMESTAMP' then 'java.util.Date'
+    when upper(substr(data_type, 1,9)) = 'TIMESTAMP' then 'java.util.Date'
     when upper(data_type) = 'FLOAT' then 'float'
     when upper(data_type) = 'VARCHAR2' then 'String'
     when upper(data_type) = 'NVARCHAR2' then 'String'
