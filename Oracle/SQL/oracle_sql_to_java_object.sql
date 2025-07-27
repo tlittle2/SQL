64658960ff5941b157fc0c99e608966b60f5e370
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-27 10:17:03 -0400

    Create oracle_sql_to_java_object.sql

---
+++ Oracle/SQL/oracle_sql_to_java_object.sql
@@ -0,0 +1,38 @@
+--camel case
+with ds as(
+select column_name, data_type, data_length, data_precision,replace(initcap(replace(column_name, '_', ' ')), ' ') as converted
+from all_tab_columns
+where table_name = 'ERROR_LOG'
+order by column_id asc
+)
+
+select 'private ' || case 
+    when upper(data_type) = 'CHAR' then 'char'
+    when upper(data_type) = 'NUMBER' and nvl(data_precision,0) = 0 then 'int'
+    when upper(data_type) = 'NUMBER' and nvl(data_precision,0) > 0 then 'double'
+    when upper(data_type) = 'INTEGER' then 'int'
+    when upper(data_type) = 'DATE' then 'java.util.Date'
+    when upper(data_type) = 'TIMESTAMP' then 'java.util.Date'
+    when upper(data_type) = 'FLOAT' then 'float'
+    when upper(data_type) = 'VARCHAR2' then 'String'
+    when upper(data_type) = 'NVARCHAR2' then 'String'
+    end || ' ' || replace(converted, substr(converted,1,1), lower(substr(converted,1,1))) || ';' as java_type 
+ from ds;
+
+
+--original
+select column_name, data_type, data_length, data_precision
+, 'private ' || case 
+    when upper(data_type) = 'CHAR' then 'char'
+    when upper(data_type) = 'NUMBER' and nvl(data_precision,0) = 0 then 'int'
+    when upper(data_type) = 'NUMBER' and nvl(data_precision,0) > 0 then 'double'
+    when upper(data_type) = 'INTEGER' then 'int'
+    when upper(data_type) = 'DATE' then 'java.util.Date'
+    when upper(data_type) = 'TIMESTAMP' then 'java.util.Date'
+    when upper(data_type) = 'FLOAT' then 'float'
+    when upper(data_type) = 'VARCHAR2' then 'String'
+    when upper(data_type) = 'NVARCHAR2' then 'String'
+    end || ' ' || replace(initcap(replace(column_name, '_', ' ')), ' ') || ';' as java_type 
+ from all_tab_columns
+where table_name = 'SALARY_DATA_STG'
+order by column_id asc;
