91146668337363d0508430664873b0a989f49410
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-27 12:07:22 -0400

    Update generate_select_into_rowtype_procedure_for_a_table.sql

--- Oracle/SQL/generate_select_into_rowtype_procedure_for_a_table.sql
+++ Oracle/SQL/generate_select_into_rowtype_procedure_for_a_table.sql
@@ -37,6 +37,28 @@ and constraints.constraint_type = 'P'
 GROUP BY columns.TABLE_NAME
 );
 
+--by rowid AND primary key
+select lower(stmnt) from (
+select 'procedure get_' || columns.table_name || '_rowid' -- change number here if you want multiple "getter" procedures
+|| '(p_rowid IN rowid, '
+|| LISTAGG('p_' || columns.COLUMN_NAME || ' IN ' || columns.table_name || '.' || columns.column_name || '%type', ',') WITHIN GROUP (ORDER BY columns.position)
+|| ',p_' || columns.table_name || '_row IN OUT ' || columns.table_name || '%rowtype'
+|| ')'
+|| 'is begin select * into  p_' || columns.table_name || '_row' || ' from ' || columns.table_name
+|| ' WHERE '
+|| LISTAGG(columns.COLUMN_NAME || '=' || 'p_' || columns.COLUMN_NAME, ' and ') WITHIN GROUP (ORDER BY columns.position)
+|| ' and  rowid = p_rowid'
+|| '; exception when no_data_found then raise; when too_many_rows then raise; when others then raise; end get_' || columns.table_name || '_1;' -- change number here if you want multiple "getter" procedures
+as stmnt
+from user_constraints constraints
+inner join user_cons_columns columns
+on constraints.table_name  = columns.table_name
+and constraints.constraint_name = columns.constraint_name
+where constraints.table_name = 'SALARY_DATA_STG'
+and constraints.constraint_type = 'P'
+GROUP BY columns.TABLE_NAME
+);
+
 
 
 --for indexed columns
