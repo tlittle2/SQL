e9b0f322162e3584576120e66117b051c1ce8e14
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-27 11:19:52 -0400

    Update oracle_sql_to_java_object.sql

--- Oracle/SQL/oracle_sql_to_java_object.sql
+++ Oracle/SQL/oracle_sql_to_java_object.sql
@@ -24,7 +24,28 @@ select 'private ' || case
  from ds;
 
 
+--Create Java Record out of datatypes
+with ds as(
+select column_name, data_type, data_length, data_precision,replace(initcap(replace(column_name, '_', ' ')), ' ') as converted
+from all_tab_columns
+where table_name = 'SALARY_DATA_STG'
+--and column_name in ('CASE_NUM' , 'FIELD' , 'YEAR' , 'SALARY')
+order by column_id asc
+)
 
+select case 
+    when upper(data_type) = 'CHAR' then 'char'
+    when upper(data_type) = 'NUMBER' and nvl(data_precision,0) = 0 then
+        case when data_length <= 10 then 'int' else 'double' end
+    when upper(data_type) = 'NUMBER' and nvl(data_precision,0) > 0 then 'double'
+    when upper(data_type) = 'INTEGER' then 'int'
+    when upper(data_type) = 'DATE' then 'java.util.Date'
+    when upper(substr(data_type, 1,9)) = 'TIMESTAMP' then 'java.util.Date'
+    when upper(data_type) = 'FLOAT' then 'float'
+    when upper(data_type) = 'VARCHAR2' then 'String'
+    when upper(data_type) = 'NVARCHAR2' then 'String'
+    end || ' ' || replace(converted, substr(converted,1,1), lower(substr(converted,1,1))) || ',' as java_type 
+ from ds;
 
 --original
 select column_name, data_type, data_length, data_precision
