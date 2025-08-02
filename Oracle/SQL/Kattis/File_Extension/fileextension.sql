89012b91c26e0a2c3b46db4f0d7efc1d5d1b314f
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-08-02 13:24:21 -0400

    Update fileextension.sql

--- Oracle/SQL/Kattis/File_Extension/fileextension.sql
+++ Oracle/SQL/Kattis/File_Extension/fileextension.sql
@@ -3,4 +3,6 @@ with ds as(
 select 'model.blend' as word from dual
 )
 
-select word, reverse(substr(reverse(word), 1, instr(reverse(word), '.'))) from ds;
+select reverse(substr(reverse(word), 1, instr(reverse(word), '.')))
+--, word
+from ds;
