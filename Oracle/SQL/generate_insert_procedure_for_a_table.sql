58f4dca3424cd4844c4211dceed05fe18d2306d5
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-08-09 16:05:38 -0400

    Update generate_insert_procedure_for_a_table.sql

--- Oracle/SQL/generate_insert_procedure_for_a_table.sql
+++ Oracle/SQL/generate_insert_procedure_for_a_table.sql
@@ -1,3 +1,18 @@
+--for a %rowtype variable
+select lower(stmnt) from (
+select 'procedure insert_' || table_name || '_row(p_row ' || tab.table_name  || '%rowtype)'
+|| 'is begin INSERT INTO '
+|| table_name
+|| ' VALUES '
+|| '('
+|| LISTAGG('p_row.' || COLUMN_NAME, ' , ')  WITHIN GROUP (ORDER BY COLUMN_ID)
+|| '); exception when others then raise; end insert_' || table_name || ';' as stmnt
+from user_tab_columns tab
+where tab.table_name in ('SALARY_DATA_STG', 'CONTROL_REPS')
+GROUP BY tab.TABLE_NAME
+);
+
+--every column accounted for in parameter
 select lower(stmnt) from (
 select 'procedure insert_' || table_name || '(' || LISTAGG('p_' || COLUMN_NAME || ' ' || table_name || '.' || column_name || '%type DEFAULT NULL', ' , ') WITHIN GROUP (ORDER BY COLUMN_ID) || ')'
 || 'is begin INSERT INTO '
