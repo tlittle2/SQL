328c1b95cd6c18e78d4265a469b1cae9c8dbb2c8
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-08-09 17:43:17 -0400

    Update generate_delete_procedure_for_a_table.sql

--- Oracle/SQL/generate_delete_procedure_for_a_table.sql
+++ Oracle/SQL/generate_delete_procedure_for_a_table.sql
@@ -1,4 +1,4 @@
---delete by rowid
+--=======================================delete by rowid=======================================
 select lower(stmnt) from (
 select 'procedure delete_' || columns.table_name || '_rowid(p_rowid IN rowid)'
 || 'is begin DELETE FROM ' || columns.table_name || ' WHERE rowid = p_rowid'
@@ -13,6 +13,7 @@ and constraints.constraint_type = 'P'
 GROUP BY columns.TABLE_NAME
 );
 
+--=======================================delete by rowid=======================================
 
 
 --=======================================primary key columns=======================================
@@ -74,7 +75,7 @@ from user_tables tab
 --=======================================primary key columns=======================================
 
 
---primary key columns given input %rowtype
+--=======================================primary key columns given input %rowtype=======================================
 select lower(stmnt) from (
 select 'procedure delete_' || columns.table_name || '_row(p_row IN ' || columns.table_name  || '%rowtype)'
 || 'is begin DELETE FROM ' || columns.table_name || ' WHERE ' || LISTAGG(columns.COLUMN_NAME || '=' || 'p_row.' || columns.COLUMN_NAME, ' and ') WITHIN GROUP (ORDER BY columns.position)
@@ -90,10 +91,12 @@ GROUP BY columns.TABLE_NAME
 );
 
 
+--=======================================primary key columns given input %rowtype=======================================
 
 
 
---indexed columns
+--=======================================indexed columns=======================================
+
 select lower(stmnt) from (
 select 'procedure delete_' || columns.table_name || '_3(' || LISTAGG('p_' || columns.COLUMN_NAME || ' IN ' || columns.table_name || '.' || columns.column_name || '%type', ' , ') WITHIN GROUP (ORDER BY columns.COLUMN_POSITION) || ')'
 || 'is begin DELETE FROM ' || columns.table_name || ' WHERE ' || LISTAGG(columns.COLUMN_NAME || '=' || 'p_' || columns.COLUMN_NAME, ' and ') WITHIN GROUP (ORDER BY columns.COLUMN_POSITION)
@@ -104,7 +107,11 @@ where columns.table_name = 'ARCHIVE_RULES'
 GROUP BY columns.TABLE_NAME
 );
 
---indexed columns given input rowtype
+--=======================================indexed columns=======================================
+
+
+
+--=======================================indexed columns given input rowtype=======================================
 select lower(stmnt) from (
 select 'procedure delete_' || columns.table_name || '_row(p_row IN ' || columns.table_name||'%rowtype)'
 || 'is begin DELETE FROM ' || columns.table_name || ' WHERE ' || LISTAGG(columns.COLUMN_NAME || '=' || 'p_row.' || columns.COLUMN_NAME, ' and ') WITHIN GROUP (ORDER BY columns.COLUMN_POSITION)
@@ -115,8 +122,12 @@ where columns.table_name = 'ARCHIVE_RULES'
 GROUP BY columns.TABLE_NAME
 );
 
+--=======================================indexed columns given input rowtype=======================================
 
---custom columns
+
+--=======================================custom columns=======================================
+
+--
 select lower(stmnt) from (
 select 'procedure delete_' || columns.table_name || '(' || LISTAGG('p_' || columns.COLUMN_NAME || ' IN ' || columns.table_name || '.' || columns.column_name || '%type', ' , ') WITHIN GROUP (ORDER BY columns.column_Id) || ')'
 || 'is begin DELETE FROM ' || columns.table_name || ' WHERE ' || LISTAGG(columns.COLUMN_NAME || '=' || 'p_' || columns.COLUMN_NAME, ' and ') WITHIN GROUP (ORDER BY columns.column_Id)
@@ -128,7 +139,11 @@ and columns.column_name in ('TABLE_OWNER', 'YEARS_TO_KEEP')
 GROUP BY columns.TABLE_NAME
 );
 
---custom columns given rowtype
+--=======================================custom columns=======================================
+
+
+--=======================================custom columns given rowtype=======================================
+
 select lower(stmnt) from (
 select 'procedure delete_' || columns.table_name || '_row(p_row IN ' || columns.TABLE_NAME || '%rowtype)'
 || 'is begin DELETE FROM ' || columns.table_name || ' WHERE ' || LISTAGG(columns.COLUMN_NAME || '=' || 'p_row.' || columns.COLUMN_NAME, ' and ') WITHIN GROUP (ORDER BY columns.column_Id)
@@ -140,3 +155,4 @@ and columns.column_name in ('TABLE_OWNER', 'YEARS_TO_KEEP')
 GROUP BY columns.TABLE_NAME
 );
 
+--=======================================custom columns given rowtype=======================================
