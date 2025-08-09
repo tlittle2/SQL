cb556eb8b3f9515d3e5a60a106b7978d353f1d8a
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-08-09 19:22:09 -0400

    Update generate_update_procedure_for_a_table.sql

--- Oracle/SQL/generate_update_procedure_for_a_table.sql
+++ Oracle/SQL/generate_update_procedure_for_a_table.sql
@@ -40,6 +40,38 @@ from user_tables tab
 
 --======================================================generic update for all columns======================================================
 
+--======================================================update nvl based on %rowtype======================================================
+
+select lower(stmnt) from (
+select 'procedure update_' || tab.table_name
+|| '_row(p_row IN ' || tab.table_name || '%rowtype)'
+|| 'is begin UPDATE '
+|| tab.table_name || ' set ' || LISTAGG(tab.COLUMN_NAME || ' = nvl(' ||'p_row.' || tab.COLUMN_NAME || ',' || tab.column_name || ')', ' , ') WITHIN GROUP (ORDER BY tab.COLUMN_ID)
+|| '; exception when others then raise; end update_' || tab.table_name || '_row;' as stmnt
+from user_tab_columns tab
+where tab.table_name in ('SALARY_DATA_STG', 'ASTROLOGY', 'CONTROL_REPS')
+GROUP BY tab.TABLE_NAME
+);
+
+select case when s_order = 2 and nxt = 3 then substr(stmnt, 1, length(stmnt) -1) else stmnt end as stmnt from (
+select s_order, lead(s_order, 1) over (order by table_name,s_order asc) as nxt, lower(stmnt) as stmnt from (
+select 1 as s_order, table_name as table_name, null as column_name, 0 as column_id, 'procedure update_' || tab.table_name|| '_row(p_row IN ' || tab.table_name || '%rowtype)'|| 'is begin UPDATE '|| tab.table_name || ' set ' as stmnt
+from user_tables tab
+
+union all
+
+select 2 as s_order, table_name as table_name, column_name as column_name, column_id as column_id, column_name || ' = nvl(' ||'p_row.' || tab.COLUMN_NAME || ',' || tab.column_name || '),' stmnt
+from user_tab_columns tab
+
+union all
+
+select 3 as s_order, table_name as table_name, null as column_name, 32767 as column_id, '; exception when others then raise; end update_' || tab.table_name || '_row;' as stmnt
+from user_tables tab
+)order by table_name, s_order, column_id
+);
+
+--======================================================update nvl based on %rowtype======================================================
+
 --======================================================generic update based on pl/sql row (update table set row = p_row)======================================================
 
 select lower(stmnt) from (
@@ -55,6 +87,18 @@ GROUP BY tab.TABLE_NAME
 
 --======================================================generic update based on pl/sql row (update table set row = p_row)======================================================
 
+--======================================================update set row based on rowid given a %rowtype ======================================================
+select lower(stmnt) from (
+select 'procedure update_' || tab.table_name
+|| '_rowid( p_rowid IN rowid, p_row IN ' || tab.table_name || '%rowtype)'
+|| 'is begin UPDATE '
+|| tab.table_name || ' set row = p_row'
+|| ' where rowid = p_rowid'
+|| '; exception when others then raise; end update_' || tab.table_name || ';' as stmnt
+from user_tables tab
+);
+
+--======================================================update set row based on rowid given a %rowtype ======================================================
 
 
 
@@ -104,19 +148,6 @@ from user_tables tab
 --======================================================update by rowid======================================================
 
 
---======================================================update set row based on rowid given a %rowtype ======================================================
-select lower(stmnt) from (
-select 'procedure update_' || tab.table_name
-|| '_rowid( p_rowid IN rowid, p_row IN ' || tab.table_name || '%rowtype)'
-|| 'is begin UPDATE '
-|| tab.table_name || ' set row = p_row'
-|| ' where rowid = p_rowid'
-|| '; exception when others then raise; end update_' || tab.table_name || ';' as stmnt
-from user_tables tab
-);
-
---======================================================update set row based on rowid given a %rowtype ======================================================
-
 
 
 
@@ -153,36 +184,3 @@ group by tbl
 
 
 --======================================================where clause is primary key columns (with primary key columns at the front)======================================================
-
-
---======================================================update nvl based on %rowtype======================================================
-
-select lower(stmnt) from (
-select 'procedure update_' || tab.table_name
-|| '_row(p_row IN ' || tab.table_name || '%rowtype)'
-|| 'is begin UPDATE '
-|| tab.table_name || ' set ' || LISTAGG(tab.COLUMN_NAME || ' = nvl(' ||'p_row.' || tab.COLUMN_NAME || ',' || tab.column_name || ')', ' , ') WITHIN GROUP (ORDER BY tab.COLUMN_ID)
-|| '; exception when others then raise; end update_' || tab.table_name || '_row;' as stmnt
-from user_tab_columns tab
-where tab.table_name in ('SALARY_DATA_STG', 'ASTROLOGY', 'CONTROL_REPS')
-GROUP BY tab.TABLE_NAME
-);
-
-select case when s_order = 2 and nxt = 3 then substr(stmnt, 1, length(stmnt) -1) else stmnt end as stmnt from (
-select s_order, lead(s_order, 1) over (order by table_name,s_order asc) as nxt, lower(stmnt) as stmnt from (
-select 1 as s_order, table_name as table_name, null as column_name, 0 as column_id, 'procedure update_' || tab.table_name|| '_row(p_row IN ' || tab.table_name || '%rowtype)'|| 'is begin UPDATE '|| tab.table_name || ' set ' as stmnt
-from user_tables tab
-
-union all
-
-select 2 as s_order, table_name as table_name, column_name as column_name, column_id as column_id, column_name || ' = nvl(' ||'p_row.' || tab.COLUMN_NAME || ',' || tab.column_name || '),' stmnt
-from user_tab_columns tab
-
-union all
-
-select 3 as s_order, table_name as table_name, null as column_name, 32767 as column_id, '; exception when others then raise; end update_' || tab.table_name || '_row;' as stmnt
-from user_tables tab
-)order by table_name, s_order, column_id
-);
-
---======================================================update nvl based on %rowtype======================================================
