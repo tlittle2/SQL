d3a3a43c5fef34ee04b52f576171d995c4051a55
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-08-09 19:38:34 -0400

    Update generate_delete_procedure_for_a_table.sql

--- Oracle/SQL/generate_delete_procedure_for_a_table.sql
+++ Oracle/SQL/generate_delete_procedure_for_a_table.sql
@@ -80,23 +80,7 @@ from user_tables tab
 --=======================================primary key columns=======================================
 
 
---=======================================primary key columns given input %rowtype=======================================
-select lower(stmnt) from (
-select 'procedure delete_' || columns.table_name || '_row(p_row IN ' || columns.table_name  || '%rowtype)'
-|| 'is begin DELETE FROM ' || columns.table_name || ' WHERE ' || LISTAGG(columns.COLUMN_NAME || '=' || 'p_row.' || columns.COLUMN_NAME, ' and ') WITHIN GROUP (ORDER BY columns.position)
-|| '; exception when others then raise; end delete_' || columns.table_name || ';'
-as stmnt
-from user_constraints constraints
-inner join user_cons_columns columns
-on constraints.table_name  = columns.table_name
-and constraints.constraint_name = columns.constraint_name
-where constraints.table_name = 'ARCHIVE_RULES'
-and constraints.constraint_type = 'P'
-GROUP BY columns.TABLE_NAME
-);
 
-
---=======================================primary key columns given input %rowtype=======================================
 
 
 
@@ -116,18 +100,6 @@ GROUP BY columns.TABLE_NAME
 
 
 
---=======================================indexed columns given input rowtype=======================================
-select lower(stmnt) from (
-select 'procedure delete_' || columns.table_name || '_row(p_row IN ' || columns.table_name||'%rowtype)'
-|| 'is begin DELETE FROM ' || columns.table_name || ' WHERE ' || LISTAGG(columns.COLUMN_NAME || '=' || 'p_row.' || columns.COLUMN_NAME, ' and ') WITHIN GROUP (ORDER BY columns.COLUMN_POSITION)
-|| '; exception when others then raise; end delete_' || columns.table_name || ';'
-as stmnt
-from user_ind_columns columns
-where columns.table_name = 'ARCHIVE_RULES'
-GROUP BY columns.TABLE_NAME
-);
-
---=======================================indexed columns given input rowtype=======================================
 
 
 --=======================================custom columns=======================================
@@ -146,17 +118,3 @@ GROUP BY columns.TABLE_NAME
 --=======================================custom columns=======================================
 
 
---=======================================custom columns given rowtype=======================================
-
-select lower(stmnt) from (
-select 'procedure delete_' || columns.table_name || '_row(p_row IN ' || columns.TABLE_NAME || '%rowtype)'
-|| 'is begin DELETE FROM ' || columns.table_name || ' WHERE ' || LISTAGG(columns.COLUMN_NAME || '=' || 'p_row.' || columns.COLUMN_NAME, ' and ') WITHIN GROUP (ORDER BY columns.column_Id)
-|| '; exception when others then raise; end delete_' || columns.table_name || ';'
-as stmnt
-from user_tab_columns columns
-where columns.table_name = 'ARCHIVE_RULES'
-and columns.column_name in ('TABLE_OWNER', 'YEARS_TO_KEEP')
-GROUP BY columns.TABLE_NAME
-);
-
---=======================================custom columns given rowtype=======================================
