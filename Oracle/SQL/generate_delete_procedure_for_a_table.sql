6d26f731953c1225988bdf9641c75de4c59688f0
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-08-09 16:19:42 -0400

    Update generate_delete_procedure_for_a_table.sql

--- Oracle/SQL/generate_delete_procedure_for_a_table.sql
+++ Oracle/SQL/generate_delete_procedure_for_a_table.sql
@@ -32,7 +32,7 @@ GROUP BY columns.TABLE_NAME
 --primary key columns given input %rowtype
 select lower(stmnt) from (
 select 'procedure delete_' || columns.table_name || '_row(p_row ' || columns.table_name  || '%rowtype)'
-|| 'is begin DELETE FROM ' || columns.table_name || ' WHERE ' || LISTAGG(columns.COLUMN_NAME || '=' || 'p_row' || columns.COLUMN_NAME, ' and ') WITHIN GROUP (ORDER BY columns.position)
+|| 'is begin DELETE FROM ' || columns.table_name || ' WHERE ' || LISTAGG(columns.COLUMN_NAME || '=' || 'p_row.' || columns.COLUMN_NAME, ' and ') WITHIN GROUP (ORDER BY columns.position)
 || '; exception when others then raise; end delete_' || columns.table_name || ';'
 as stmnt
 from user_constraints constraints
@@ -43,6 +43,7 @@ where constraints.table_name = 'ARCHIVE_RULES'
 and constraints.constraint_type = 'P'
 GROUP BY columns.TABLE_NAME
 );
+
 
 
 --indexed columns
