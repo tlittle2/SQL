2a32367aa362fe8e7359c21e2b3ff31895b1e96b
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-08-09 16:32:15 -0400

    Update generate_insert_procedure_for_a_table.sql

--- Oracle/SQL/generate_insert_procedure_for_a_table.sql
+++ Oracle/SQL/generate_insert_procedure_for_a_table.sql
@@ -25,3 +25,24 @@ from user_tab_columns
 where table_name = 'ASTROLOGY' --modify procedure accordingly for your particular where clause
 GROUP BY TABLE_NAME
 );
+
+
+--===========================if the table is too wide (list_agg()) does have its limitations===========================
+
+--select lower(stmnt) from (
+select 'procedure insert_' || table_name || '_row(p_row IN ' || tab.table_name  || '%rowtype) is begin INSERT INTO ' || tab.table_name || ' VALUES (' as stmnt
+from all_tables tab
+where tab.table_name in ('SALARY_DATA_STG')
+
+union all
+
+select 'p_row.' || tab.column_name || ','
+from user_tab_columns tab
+where tab.table_name in ('SALARY_DATA_STG')
+
+union all
+
+select '); exception when others then raise; end insert_' || table_name || ';' 
+from all_tables tab
+where tab.table_name in ('SALARY_DATA_STG')
+);
