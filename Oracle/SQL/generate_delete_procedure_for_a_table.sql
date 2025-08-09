42caa482610f9cfdd3b422b4fe34d83e382e233a
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-08-09 16:18:12 -0400

    Update generate_delete_procedure_for_a_table.sql

--- Oracle/SQL/generate_delete_procedure_for_a_table.sql
+++ Oracle/SQL/generate_delete_procedure_for_a_table.sql
@@ -29,11 +29,37 @@ and constraints.constraint_type = 'P'
 GROUP BY columns.TABLE_NAME
 );
 
+--primary key columns given input %rowtype
+select lower(stmnt) from (
+select 'procedure delete_' || columns.table_name || '_row(p_row ' || columns.table_name  || '%rowtype)'
+|| 'is begin DELETE FROM ' || columns.table_name || ' WHERE ' || LISTAGG(columns.COLUMN_NAME || '=' || 'p_row' || columns.COLUMN_NAME, ' and ') WITHIN GROUP (ORDER BY columns.position)
+|| '; exception when others then raise; end delete_' || columns.table_name || ';'
+as stmnt
+from user_constraints constraints
+inner join user_cons_columns columns
+on constraints.table_name  = columns.table_name
+and constraints.constraint_name = columns.constraint_name
+where constraints.table_name = 'ARCHIVE_RULES'
+and constraints.constraint_type = 'P'
+GROUP BY columns.TABLE_NAME
+);
+
 
 --indexed columns
 select lower(stmnt) from (
 select 'procedure delete_' || columns.table_name || '_3(' || LISTAGG('p_' || columns.COLUMN_NAME || ' IN ' || columns.table_name || '.' || columns.column_name || '%type', ' , ') WITHIN GROUP (ORDER BY columns.COLUMN_POSITION) || ')'
 || 'is begin DELETE FROM ' || columns.table_name || ' WHERE ' || LISTAGG(columns.COLUMN_NAME || '=' || 'p_' || columns.COLUMN_NAME, ' and ') WITHIN GROUP (ORDER BY columns.COLUMN_POSITION)
+|| '; exception when others then raise; end delete_' || columns.table_name || ';'
+as stmnt
+from user_ind_columns columns
+where columns.table_name = 'ARCHIVE_RULES'
+GROUP BY columns.TABLE_NAME
+);
+
+--indexed columns given input rowtype
+select lower(stmnt) from (
+select 'procedure delete_' || columns.table_name || '_row(p_row ' || columns.table_name||'%rowtype)'
+|| 'is begin DELETE FROM ' || columns.table_name || ' WHERE ' || LISTAGG(columns.COLUMN_NAME || '=' || 'p_row.' || columns.COLUMN_NAME, ' and ') WITHIN GROUP (ORDER BY columns.COLUMN_POSITION)
 || '; exception when others then raise; end delete_' || columns.table_name || ';'
 as stmnt
 from user_ind_columns columns
@@ -53,3 +79,17 @@ where columns.table_name = 'ARCHIVE_RULES'
 and columns.column_name = 'TABLE_OWNER'
 GROUP BY columns.TABLE_NAME
 );
+
+--custom columns given rowtype
+
+select lower(stmnt) from (
+select 'procedure delete_' || columns.table_name || '_row(p_row ' || columns.TABLE_NAME || '%rowtype)'
+|| 'is begin DELETE FROM ' || columns.table_name || ' WHERE ' || LISTAGG(columns.COLUMN_NAME || '=' || 'p_row.' || columns.COLUMN_NAME, ' and ') WITHIN GROUP (ORDER BY columns.column_Id)
+|| '; exception when others then raise; end delete_' || columns.table_name || ';'
+as stmnt
+from user_tab_columns columns
+where columns.table_name = 'ARCHIVE_RULES'
+and columns.column_name = 'TABLE_OWNER'
+GROUP BY columns.TABLE_NAME
+);
+
