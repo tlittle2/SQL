4c292c2f90787028f6b672c5fab36fec01ee9f60
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-08-09 20:13:09 -0400

    Update generate_insert_procedure_for_a_table.sql

--- Oracle/SQL/generate_insert_procedure_for_a_table.sql
+++ Oracle/SQL/generate_insert_procedure_for_a_table.sql
@@ -1,17 +1,3 @@
---for a %rowtype variable
-select lower(stmnt) from (
-select 'procedure insert_' || table_name || '_row(p_row IN ' || tab.table_name  || '%rowtype)'
-|| 'is begin INSERT INTO '
-|| table_name
-|| ' VALUES '
-|| '('
-|| LISTAGG('p_row.' || COLUMN_NAME, ' , ')  WITHIN GROUP (ORDER BY COLUMN_ID)
-|| '); exception when others then raise; end insert_' || table_name || ';' as stmnt
-from user_tab_columns tab
-where tab.table_name in ('SALARY_DATA_STG', 'CONTROL_REPS')
-GROUP BY tab.TABLE_NAME
-);
-
 --every column accounted for in parameter
 select lower(stmnt) from (
 select 'procedure insert_' || table_name || '(' || LISTAGG('p_' || COLUMN_NAME || ' ' || table_name || '.' || column_name || '%type DEFAULT NULL', ' , ') WITHIN GROUP (ORDER BY COLUMN_ID) || ')'
@@ -26,23 +12,3 @@ where table_name = 'ASTROLOGY' --modify procedure accordingly for your particula
 GROUP BY TABLE_NAME
 );
 
-
---===========================if the table is too wide (list_agg()) does have its limitations===========================
-
---select lower(stmnt) from (
-select 'procedure insert_' || table_name || '_row(p_row IN ' || tab.table_name  || '%rowtype) is begin INSERT INTO ' || tab.table_name || ' VALUES (' as stmnt
-from all_tables tab
-where tab.table_name in ('SALARY_DATA_STG')
-
-union all
-
-select 'p_row.' || tab.column_name || ','
-from user_tab_columns tab
-where tab.table_name in ('SALARY_DATA_STG')
-
-union all
-
-select '); exception when others then raise; end insert_' || table_name || ';' 
-from all_tables tab
-where tab.table_name in ('SALARY_DATA_STG')
-);
