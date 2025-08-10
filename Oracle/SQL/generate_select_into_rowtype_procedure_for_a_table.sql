ca18fbbefa73b0867fbb9bc33da6fa0750126cf1
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-08-09 20:37:38 -0400

    Update generate_select_into_rowtype_procedure_for_a_table.sql

--- Oracle/SQL/generate_select_into_rowtype_procedure_for_a_table.sql
+++ Oracle/SQL/generate_select_into_rowtype_procedure_for_a_table.sql
@@ -38,9 +38,11 @@ and constraints.constraint_type = 'P'
 GROUP BY columns.TABLE_NAME
 );
 
+
+
 --by rowid AND primary key
 select lower(stmnt) from (
-select 'procedure get_' || columns.table_name || '_rowid_pk' -- change number here if you want multiple "getter" procedures
+select 'procedure get_' || columns.table_name || '_' || constraints.constraint_name || '_rowid' -- '_1' change number here if you want multiple "getter" procedures
 || '(p_' || columns.table_name || '_row IN OUT ' || columns.table_name || '%rowtype,'
 || 'p_rowid IN rowid, '
 || LISTAGG('p_' || columns.COLUMN_NAME || ' IN ' || columns.table_name || '.' || columns.column_name || '%type', ',') WITHIN GROUP (ORDER BY columns.position)
@@ -57,7 +59,7 @@ on constraints.table_name  = columns.table_name
 and constraints.constraint_name = columns.constraint_name
 where constraints.table_name = 'SALARY_DATA_STG'
 and constraints.constraint_type = 'P'
-GROUP BY columns.TABLE_NAME
+GROUP BY columns.TABLE_NAME,constraints.constraint_name
 );
 
 
@@ -65,17 +67,19 @@ GROUP BY columns.TABLE_NAME
 
 --for indexed columns
 select lower(stmnt) from (
-select 'procedure get_' || columns.table_name || '_2' -- change number here if you want multiple "getter" procedures
+select 'procedure get_' || columns.table_name || '_' || columns.index_name --'_2' change number here if you want multiple "getter" procedures
 || '('
 || 'p_' || columns.table_name || '_row IN OUT ' || columns.table_name || '%rowtype,'
+|| 'p_rowid IN rowid,'
 || LISTAGG('p_' || columns.COLUMN_NAME || ' IN ' || columns.table_name || '.' || columns.column_name || '%type', ',') WITHIN GROUP (ORDER BY columns.COLUMN_POSITION)
 || ')'
 || 'is begin select * into  p_' || columns.table_name || '_row' || ' from ' || columns.table_name || ' WHERE ' || LISTAGG(columns.COLUMN_NAME || '=' || 'p_' || columns.COLUMN_NAME, ' and ') WITHIN GROUP (ORDER BY columns.COLUMN_POSITION)
-|| '; exception when no_data_found then raise; when too_many_rows then raise; when others then raise; end get_' || columns.table_name || '_2;' -- change number here if you want multiple "getter" procedures
+|| ' and rowid = p_rowid'
+|| '; exception when no_data_found then raise; when too_many_rows then raise; when others then raise; end get_'  || columns.table_name || '_' || columns.index_name ||';'--'_2;'  change number here if you want multiple "getter" procedures
 as stmnt
 from user_ind_columns columns
 where columns.table_name in('DIM_APP_TABLES', 'UPDATE_MATCH')
-GROUP BY columns.TABLE_NAME
+GROUP BY columns.TABLE_NAME , columns.index_name
 );
 
 --indexed columns AND rowid
