7cb18889d7b749ae13eac43e3db7ab8e41501da2
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-08-09 20:09:27 -0400

    Update generate_update_rowtype_procedure_for_a_table.sql

--- Oracle/SQL/generate_update_rowtype_procedure_for_a_table.sql
+++ Oracle/SQL/generate_update_rowtype_procedure_for_a_table.sql
@@ -1,16 +1,5 @@
 --======================================================generic update nvl based on %rowtype======================================================
 
-select lower(stmnt) from (
-select 'procedure update_' || tab.table_name
-|| '_row(p_row IN ' || tab.table_name || '%rowtype)'
-|| 'is begin UPDATE '
-|| tab.table_name || ' set ' || LISTAGG(tab.COLUMN_NAME || ' = nvl(' ||'p_row.' || tab.COLUMN_NAME || ',' || tab.column_name || ')', ' , ') WITHIN GROUP (ORDER BY tab.COLUMN_ID)
-|| '; exception when others then raise; end update_' || tab.table_name || '_row;' as stmnt
-from user_tab_columns tab
-where tab.table_name in ('SALARY_DATA_STG', 'ASTROLOGY', 'CONTROL_REPS')
-GROUP BY tab.TABLE_NAME
-);
-
 select case when s_order = 2 and nxt = 3 then substr(stmnt, 1, length(stmnt) -1) else stmnt end as stmnt from (
 select s_order, lead(s_order, 1) over (order by table_name,s_order asc) as nxt, lower(stmnt) as stmnt from (
 select 1 as s_order, table_name as table_name, null as column_name, 0 as column_id, 'procedure update_' || tab.table_name|| '_row(p_row IN ' || tab.table_name || '%rowtype)'|| 'is begin UPDATE '|| tab.table_name || ' set ' as stmnt
@@ -26,6 +15,17 @@ union all
 select 3 as s_order, table_name as table_name, null as column_name, 32767 as column_id, '; exception when others then raise; end update_' || tab.table_name || '_row;' as stmnt
 from user_tables tab
 )order by table_name, s_order, column_id
+);
+
+select lower(stmnt) from (
+select 'procedure update_' || tab.table_name
+|| '_row(p_row IN ' || tab.table_name || '%rowtype)'
+|| 'is begin UPDATE '
+|| tab.table_name || ' set ' || LISTAGG(tab.COLUMN_NAME || ' = nvl(' ||'p_row.' || tab.COLUMN_NAME || ',' || tab.column_name || ')', ' , ') WITHIN GROUP (ORDER BY tab.COLUMN_ID)
+|| '; exception when others then raise; end update_' || tab.table_name || '_row;' as stmnt
+from user_tab_columns tab
+where tab.table_name in ('SALARY_DATA_STG', 'ASTROLOGY', 'CONTROL_REPS')
+GROUP BY tab.TABLE_NAME
 );
 
 --======================================================generic update nvl based on %rowtype======================================================
