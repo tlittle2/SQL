35b0a060abfdedb55662993aef8970a87b13540e
Trevor Little <trevor.j.little@gmail.com>
2025-08-16 22:50:47 -0400

    changing to concat() function instead of pipe in some instances

--- Oracle/SQL/extract_source_code.sql
+++ Oracle/SQL/extract_source_code.sql
@@ -7,10 +7,9 @@ order by name, decode(type, 'PACKAGE',1,2), line asc;
 
 --Output to Unix File
 select 'echo "' || text || '"' || ' >> ' || file_name from (
-    select name, line
-    , concat(translate(initcap(type), ' ', '_'),'s/') || decode (type, 'PACKAGE', lower(name || '.pks'), 'PACKAGE BODY', lower(name || '.pkb'), lower(name) || '.sql') as file_name
-    , decode(line, 1, 'create or replace ' || text, text) AS TEXT
-    from user_source
-    --where type = 'TYPE'
+select name, line
+, concat(translate(initcap(type), ' ', '_'),'s/') || decode(type, 'PACKAGE', lower(concat(name,'.pks')), 'PACKAGE BODY', lower(concat(name,'.pkb')), lower(concat(name, '.sql'))) as file_name
+, decode(line, 1, 'create or replace ' || text, text) AS TEXT
+from user_source
 order by name, decode(type, 'PACKAGE',1,2), line asc
 );
