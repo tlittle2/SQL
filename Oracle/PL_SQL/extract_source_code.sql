d122c33f0eea7030fd7bb9cb0b658ec2c08a0fe1
Trevor Little <trevor.j.little@gmail.com>
2025-08-16 22:18:53 -0400

    removing lowercase transformation from text field

--- Oracle/PL_SQL/extract_source_code.sql
+++ Oracle/PL_SQL/extract_source_code.sql
@@ -2,13 +2,13 @@ set serveroutput on;
 declare
 cursor code_source
 is
-select name, line, lower(text) as text
+select name, line, text as text
 from user_source where name = 'GENERATE_GUID'
 order by decode(type, 'PACKAGE',1,2), line asc;
 
 begin
 for rec in code_source
-loop   
+loop
     if rec.line = 1
     then
         dbms_output.put_line('create or replace ' || rec.text);
