a494eb02c2d536f285728ee333902b2a7b8e80a5
Trevor Little <trevor.j.little@gmail.com>
2025-08-16 22:30:09 -0400

    added enhancement for the file name of the component

--- Oracle/SQL/extract_source_code.sql
+++ Oracle/SQL/extract_source_code.sql
@@ -1,3 +1,5 @@
-select name, line, decode(line, 1 , 'create or replace ' || text, text) as text
-from user_source where name in('INFA_GLOBAL_TBL_PKG', 'MATH_PKG', 'GENERATE_GUID')
+select name, line
+, decode (type, 'PACKAGE', lower(name || '.pks'), 'PACKAGE BODY', lower(name || '.pkb'), lower(name) || '.sql') as file_name
+, decode(line, 1, 'create or replace ' || text, text) AS TEXT
+from user_source
 order by name, decode(type, 'PACKAGE',1,2), line asc;
