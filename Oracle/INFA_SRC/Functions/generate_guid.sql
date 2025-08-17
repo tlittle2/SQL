ec660b4e6c2a8d7f974e12f5eeca2b41181bf165
Trevor Little <trevor.j.little@gmail.com>
2025-08-16 22:11:40 -0400

    function to lowercase attempt 2

--- Oracle/INFA_SRC/Functions/generate_guid.sql
+++ Oracle/INFA_SRC/Functions/generate_guid.sql
@@ -1,8 +1,7 @@
 create or replace function generate_guid(p_seqnum in number, p_app_id in varchar2)
-
 return varchar2
 as
 begin
-    RETURN TO_CHAR(SYSDATE, 'yyyymmdd') || P_APP_ID || LPAD(P_SEQNUM, 12, '0');
-end;
+    return to_char(sysdate, 'yyyymmdd') || p_app_id || lpad(p_seqnum, 12, '0');
+end generate_guid;
 
