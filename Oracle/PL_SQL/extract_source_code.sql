603fd0ea21cd6dba42b4af6ac682d6338a84fc5d
Trevor Little <trevor.j.little@gmail.com>
2025-08-16 22:15:19 -0400

    adding initial thinking of anonymous block for extracting source code from database

---
+++ Oracle/PL_SQL/extract_source_code.sql
@@ -0,0 +1,20 @@
+set serveroutput on;
+declare
+cursor code_source
+is
+select name, line, lower(text) as text
+from user_source where name = 'GENERATE_GUID'
+order by decode(type, 'PACKAGE',1,2), line asc;
+
+begin
+for rec in code_source
+loop   
+    if rec.line = 1
+    then
+        dbms_output.put_line('create or replace ' || rec.text);
+    else
+        dbms_output.put_line(rec.text);
+    end if;
+end loop;
+end;
+/
