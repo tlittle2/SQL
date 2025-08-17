9e4b45c33e0d64a139a78b198b8ba9a2e656968f
Trevor Little <trevor.j.little@gmail.com>
2025-08-16 22:55:02 -0400

    adding more cases for using concat()

--- Oracle/SQL/extract_source_code.sql
+++ Oracle/SQL/extract_source_code.sql
@@ -1,6 +1,6 @@
 select name, line
-, decode (type, 'PACKAGE', lower(name || '.pks'), 'PACKAGE BODY', lower(name || '.pkb'), lower(name) || '.sql') as file_name
-, decode(line, 1, 'create or replace ' || text, text) AS TEXT
+, decode (type, 'PACKAGE', lower(concat(name,'.pks')), 'PACKAGE BODY', lower(concat(name,'.pkb')), lower(concat(name,'.sql'))) as file_name
+, decode(line, 1, concat('create or replace ',text), text) AS TEXT
 from user_source
 order by name, decode(type, 'PACKAGE',1,2), line asc;
 
@@ -9,7 +9,7 @@ order by name, decode(type, 'PACKAGE',1,2), line asc;
 select 'echo "' || text || '"' || ' >> ' || file_name from (
 select name, line
 , concat(translate(initcap(type), ' ', '_'),'s/') || decode(type, 'PACKAGE', lower(concat(name,'.pks')), 'PACKAGE BODY', lower(concat(name,'.pkb')), lower(concat(name, '.sql'))) as file_name
-, decode(line, 1, 'create or replace ' || text, text) AS TEXT
+, decode(line, 1, concat('create or replace ',text), text) AS TEXT
 from user_source
 order by name, decode(type, 'PACKAGE',1,2), line asc
 );
