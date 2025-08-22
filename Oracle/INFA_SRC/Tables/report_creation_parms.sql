08d73aa5c570bba0b81eebed765b0f89b23a920d
Trevor Little <trevor.j.little@gmail.com>
2025-08-22 17:57:10 -0400

    readding report_creation_parms.sql

--- Oracle/INFA_SRC/Tables/report_creation_parms.sql
+++ Oracle/INFA_SRC/Tables/report_creation_parms.sql
@@ -9,5 +9,4 @@ Insert into REPORT_CREATION_PARMS (REPORT_NAME,PADDING,ROLLING_HEADER,REPORT_QUE
 Insert into REPORT_CREATION_PARMS (REPORT_NAME,PADDING,ROLLING_HEADER,REPORT_QUERY) values ('RPT2',20,0,'select nvl(tablespace_name, ''(blank)'') as tablespace_name , sum(nvl(num_rows,0)) as volume from my_tables group by tablespace_name order by volume desc');
 Insert into REPORT_CREATION_PARMS (REPORT_NAME,PADDING,ROLLING_HEADER,REPORT_QUERY) values ('RPT3',20,0,'select * from astrology');
 
-
-
+commit;
