3288e667a926daaeb1a77d92b79bceb17717a32b
Trevor Little <trevor.j.little@gmail.com>
2025-08-26 22:01:37 -0400

    modifying casing and made slight change in logic for the check_schema sub function

--- Oracle/INFA_SRC/Procedures/batch_cdc.sql
+++ Oracle/INFA_SRC/Procedures/batch_cdc.sql
@@ -8,8 +8,8 @@ as
 
     cdc_list columns_list_t;
     non_cdc_list columns_list_t;
-    
-    procedure print_or_execute(p_sql IN VARCHAR2)
+
+    procedure print_or_execute(p_sql in varchar2)
     is
     begin
         if debug_pkg.get_debug_state
@@ -21,15 +21,15 @@ as
             commit;
         end if;
     end print_or_execute;
-    
+
     procedure print_extra_line
     is
     begin
         dbms_output.put_line(chr(10));
     end print_extra_line;
-    
-    
-    procedure step_separate(p_step_name in VARCHAR2)
+
+
+    procedure step_separate(p_step_name in varchar2)
     is
     begin
         dbms_output.put_line(p_step_name);
@@ -45,13 +45,14 @@ as
         end loop;
     end print_collection;
 
-    
+
     --given 2 tables, ensure that the columns are consistent
-    function check_schemas(p_source_table IN ALL_TAB_COLUMNS.TABLE_NAME%TYPE
-                          , p_target_table IN ALL_TAB_COLUMNS.TABLE_NAME%TYPE)
+    function check_schemas(p_source_table in all_tab_columns.table_name%type
+                          , p_target_table in all_tab_columns.table_name%type)
     return boolean
     IS
-        v_differences NUMBER;
+        l_differences number;
+        l_returnvalue boolean := true;
     begin
         with schema_check as (
             select a.owner, a.table_name, a.column_name
@@ -66,29 +67,29 @@ as
             WHEN 'NVARCHAR2' THEN string_utils_pkg.get_str('TEXT(%1)', a.data_length)
             WHEN 'RAW'       THEN string_utils_pkg.get_str('RAW(%1)', a.data_length)
             ELSE a.data_type end as data_type
-            
+
             from all_tab_columns a
         )
 
-            select nvl(count(1), 1) into v_differences
+            select nvl(count(1), 1) into l_differences
             from (
                 select column_name,data_type from schema_check where owner = p_table_owner and table_name = p_target_table
-                MINUS
+                minus
                 select column_name,data_type from schema_check where owner = p_table_owner and table_name = p_source_table
-                UNION ALL
+                union ALL
                 select column_name,data_type from schema_check where owner = p_table_owner and table_name = p_source_table
-                MINUS
+                minus
                 select column_name,data_type from schema_check where owner = p_table_owner and table_name = p_target_table
             );
 
-            dbms_output.put_line(v_differences);
+            dbms_output.put_line(l_differences);
 
-            if v_differences > 0 
+            if l_differences > 0 
             then
-                RETURN FALSE;
+                l_returnvalue:= false;
             end if;
 
-            RETURN TRUE;
+            return l_returnvalue;
 
     end check_schemas;
 
@@ -98,9 +99,9 @@ as
     begin
         sql_query_c1.f_select := sql_builder_pkg.g_select_all;
         sql_query_c1.f_from := sql_utils_pkg.get_full_table_name(p_table_owner,p_stage_table);
-    
+
         sql_utils_pkg.truncate_table(p_cdc_table);
-        
+
         print_or_execute(string_utils_pkg.get_str('INSERT INTO %1 %2 %3'
                          , sql_utils_pkg.get_full_table_name(p_table_owner,p_cdc_table)
                          , sql_builder_pkg.get_select(sql_query_c1)
@@ -114,8 +115,8 @@ as
         cursor cdc_columns is
             select sort_order_number
             , column_name
-        from update_match where UPPER(table_owner) = UPPER(p_table_owner)
-        and UPPER(table_name) = UPPER(p_target_table)
+        from update_match where upper(table_owner) = upper(p_table_owner)
+        and upper(table_name) = upper(p_target_table)
             order by sort_order_number asc;
     begin
 
@@ -130,13 +131,13 @@ as
     procedure gather_non_cdc_columns(p_collection in out nocopy columns_list_t)
     is
         cursor non_cdc_columns is
-        select column_id, column_name from all_tab_columns where owner = UPPER(p_table_owner) and table_name = UPPER(p_target_table)
-        AND COLUMN_NAME NOT IN ('EFF_DATE' , 'END_DATE' , 'CREATE_ID' , 'LAST_UPDATE_ID')
+        select column_id, column_name from all_tab_columns where owner = upper(p_table_owner) and table_name = upper(p_target_table)
+        and column_name not in ('EFF_DATE' , 'END_DATE' , 'CREATE_ID' , 'LAST_UPDATE_ID')
         and column_name not in (
-                  select column_name from update_match where UPPER(table_owner) = UPPER(p_table_owner) and UPPER(table_name) = UPPER(p_target_table)
+                  select column_name from update_match where upper(table_owner) = upper(p_table_owner) and upper(table_name) = upper(p_target_table)
         ) 
         order by column_id asc;
-        
+
     begin
         for rec_non_cdc in non_cdc_columns
         loop
@@ -151,23 +152,23 @@ as
         sql_query_tgt sql_builder_pkg.t_query;
         sql_query_cdc2 sql_builder_pkg.t_query;
         sql_query_where_in sql_builder_pkg.t_query;
-        
+
         v_cdc_columns_equality string_utils_pkg.st_max_pl_varchar2;
     begin
         sql_query_cdc1.f_from := sql_utils_pkg.get_full_table_name(p_table_owner,p_cdc_table);
-        
+
         sql_query_tgt.f_select := sql_builder_pkg.g_select_all;
         sql_query_tgt.f_from := sql_utils_pkg.get_full_table_name(p_table_owner,p_target_table);
-        
+
         sql_query_cdc2.f_select := '1';
         sql_query_cdc2.f_from := sql_utils_pkg.get_full_table_name(p_table_owner,p_cdc_table);
-    
+
         for i in p_cdc_columns.FIRST..p_cdc_columns.LAST
         loop
             sql_builder_pkg.add_select(sql_query_cdc1,p_cdc_columns(i)); 
             sql_builder_pkg.add_where(sql_query_tgt,p_cdc_columns(i), ','); 
             sql_builder_pkg.add_select(sql_query_where_in, p_cdc_columns(i));
-        
+
             if i <> p_cdc_columns.LAST
             then
                 string_utils_pkg.add_str_token(v_cdc_columns_equality, string_utils_pkg.get_str('b.%1 = a.%2 and ', p_cdc_columns(i), p_cdc_columns(i)), ' ');
@@ -175,16 +176,16 @@ as
                 string_utils_pkg.add_str_token(v_cdc_columns_equality, string_utils_pkg.get_str('b.%1 = a.%2', p_cdc_columns(i), p_cdc_columns(i)), ' ');
             end if;
         end loop;
-        
+
         sql_query_cdc2.f_where := v_cdc_columns_equality;
-                                                         
+
         print_or_execute('INSERT INTO '
                        || sql_query_cdc1.f_from
                        || ' '
                        || sql_builder_pkg.get_select(sql_query_tgt)
                        || sql_builder_pkg.get_from(sql_query_tgt)
                        || ' a '
-                       || sql_builder_pkg.get_where_in(sql_query_where_in, false) --false brings all data from the permanent table
+                       || sql_builder_pkg.get_where_in(sql_query_where_in, false)
                        || '('
                        || sql_builder_pkg.get_select(sql_query_cdc1)
                        || sql_builder_pkg.get_from(sql_query_cdc1)
@@ -212,27 +213,27 @@ as
             for i in p_collection.FIRST..p_collection.LAST
             LOOP
                 sql_builder_pkg.add_select(sql_query_v1,p_collection(i));
-            
+
                 if i = p_collection.LAST
                 then
                     sql_builder_pkg.add_select(sql_query_v1,'EFF_DATE) as row_id, k.*');
                     sql_builder_pkg.add_from(sql_query_v1,sql_utils_pkg.get_full_table_name(p_table_owner,p_cdc_table) || ' k)');
                 end if;
-            
+
             END LOOP;
-            
+
             string_utils_pkg.prepend_str_token(sql_query_v1.f_select, 'with vt1 as (SELECT ROW_NUMBER() OVER( ORDER BY ', '');
-            
+
             return sql_query_v1.f_select
                 || sql_builder_pkg.get_from(sql_query_v1);
 
         end create_view_1;
 
-        function CREATE_VIEW_2(p_cdc_columns IN columns_list_t, p_non_cdc_columns IN columns_list_t)
+        function create_view_2(p_cdc_columns IN columns_list_t, p_non_cdc_columns IN columns_list_t)
         return varchar2
         is
             sql_query_v2 sql_builder_pkg.t_query;
-            
+
             function dynamicJoin(p_collection IN columns_list_t)
             return VARCHAR2
             is
@@ -241,20 +242,20 @@ as
                 for i in p_collection.FIRST..p_collection.LAST
                 loop
                     sql_builder_pkg.add_where(sql_query_join, string_utils_pkg.get_str('x1.%1 = x2.%2', p_collection(i), p_collection(i)), '');
-                    
+
                     if i <> p_collection.LAST
                     then
                         string_utils_pkg.add_str_token(sql_query_join.f_where, ' and ', ''); --v_join_clause := v_join_clause || ' and ';
                     end if;
-                
+
                 end loop;
-                
+
                 string_utils_pkg.prepend_str_token(sql_query_join.f_where
                                                 , ' and '
                                                 , '');
-                        
+
                 return sql_query_join.f_where;
-            
+
             end dynamicJoin;
 
             function dynamicNotEqualClause(p_collection IN columns_list_t)
@@ -270,38 +271,38 @@ as
                                                 || '(x1.' || p_collection(i) || ' is null' || ' and ' || 'x2.' || p_collection(i) || ' is not null)'
                                                 || ' or '
                                                 || '(x1.' || p_collection(i) || ' is not null' || ' and' || ' x2.' || p_collection(i) || ' is null)';
-                                              
+
                     if i <> p_collection.LAST
                     then
                         string_utils_pkg.add_str_token(sql_query_v2_where.f_where, ' OR ', '');  
                     end if;
                 end loop;
-                
+
                 string_utils_pkg.prepend_str_token(sql_query_v2_where.f_where, ' where x2.row_id is null or ', '');
-                
+
                 return sql_query_v2_where.f_where;
-                
+
             end dynamicNotEqualClause;
 
         begin
             for i in p_cdc_columns.first..p_cdc_columns.last
             loop
                 sql_builder_pkg.add_select(sql_query_v2, 'x1.' || p_cdc_columns(i));
-                
+
                 if i = p_cdc_columns.LAST
                 then
                     sql_builder_pkg.add_select(sql_query_v2, 'x1.EFF_DATE) as row_id2, x1.* from vt1 x1 left outer join vt1 x2 on x1.row_id=x2.row_id+1');
                 end if;
             end loop;
-            
+
             string_utils_pkg.prepend_str_token(sql_query_v2.f_select, ', vt2 as (SELECT ROW_NUMBER() OVER( ORDER BY ', '');
-            
+
             return sql_query_v2.f_select
             || dynamicJoin(p_cdc_columns)
             || dynamicNotEqualClause(p_non_cdc_columns)
             || ')';
 
-        end CREATE_VIEW_2;
+        end create_view_2;
 
     begin --move_cdc_to_stage
         for i in p_cdc_columns.first..p_cdc_columns.last
@@ -314,30 +315,30 @@ as
         loop
             sql_builder_pkg.add_select(sql_query_cdc_to_stg_insert, p_non_cdc_columns(i));
             sql_builder_pkg.add_select(sql_query_cdc_to_stg_select, string_utils_pkg.get_str('x1.%1', p_non_cdc_columns(i)));
-            
+
             if i = p_non_cdc_columns.last
             THEN
                 sql_builder_pkg.add_select(sql_query_cdc_to_stg_insert, ' EFF_DATE, END_DATE, CREATE_ID, LAST_UPDATE_ID)');
                 sql_builder_pkg.add_select(sql_query_cdc_to_stg_select, 'x1.EFF_DATE, coalesce(x2.EFF_DATE-1, to_date(''31-DEC-2100'')) as END_DATE, x2.CREATE_ID, coalesce(x2.LAST_UPDATE_ID, x1.LAST_UPDATE_ID) as LAST_UPDATE_ID '
                                                                     || ' FROM vt2 x1 LEFT OUTER JOIN vt2 x2 ON ');
-                
+
                 for i in p_cdc_columns.first..p_cdc_columns.last
                 loop
                     sql_builder_pkg.add_where(sql_query_cdc_to_stg_select, string_utils_pkg.get_str('x1.%1 = x2.%2', p_cdc_columns(i), p_cdc_columns(i)), '');
-                    
+
                     if i <> p_cdc_columns.LAST
                     then
                         string_utils_pkg.add_str_token(sql_query_cdc_to_stg_select.f_where, ' and ', '');
                     end if;
                 end loop;
             end if;
-            
+
         end loop;
-        
+
         string_utils_pkg.add_str_token(sql_query_cdc_to_stg_select.f_where, 'and x1.row_id2 = x2.row_id2-1', '');
-        
+
         string_utils_pkg.prepend_str_token(sql_query_cdc_to_stg_insert.f_select, 'INSERT INTO ' || sql_utils_pkg.get_full_table_name(p_table_owner,p_stage_table) || '(', '');
-        
+
         print_or_execute(string_utils_pkg.get_str('%1 %2 %3 %4 %5'
                                 , sql_query_cdc_to_stg_insert.f_select
                                 , create_view_1(p_cdc_columns)
@@ -345,57 +346,57 @@ as
                                 , sql_builder_pkg.get_select(sql_query_cdc_to_stg_select)
                                 , sql_query_cdc_to_stg_select.f_where
                                 ));
-                                
+
     end move_cdc_to_stage;
 
     procedure remove_from_target(p_cdc_columns in columns_list_t)
     IS
-        
+
         sql_query_tgt sql_builder_pkg.t_query;
         sql_query_stg sql_builder_pkg.t_query;
-        
+
         sql_query_where_in sql_builder_pkg.t_query;
-        
+
         sql_query string_utils_pkg.st_max_pl_varchar2;
     begin
         sql_builder_pkg.add_from(sql_query_tgt, sql_utils_pkg.get_full_table_name(p_table_owner,p_target_table));
         sql_builder_pkg.add_from(sql_query_stg, sql_utils_pkg.get_full_table_name(p_table_owner,p_stage_table));
-        
+
         for i in p_cdc_columns.first..p_cdc_columns.last
         loop
             sql_builder_pkg.add_select(sql_query_where_in, p_cdc_columns(i));
         end loop;
-        
+
         print_or_execute(string_utils_pkg.get_str('DELETE %1 %2 (%3 %4)', sql_builder_pkg.get_from(sql_query_tgt), sql_builder_pkg.get_where_in(sql_query_where_in), sql_builder_pkg.get_select(sql_query_where_in), sql_builder_pkg.get_from(sql_query_stg)));
-    
+
     end remove_from_target;
-    
+
     procedure insert_to_target
     is
         sql_query_c1 sql_builder_pkg.t_query;
     begin
         sql_builder_pkg.add_select(sql_query_c1, sql_builder_pkg.g_select_all);
         sql_builder_pkg.add_from(sql_query_c1, sql_utils_pkg.get_full_table_name(p_table_owner,p_stage_table));
-    
+
         print_or_execute(string_utils_pkg.get_str('INSERT INTO %1 %2', sql_utils_pkg.get_full_table_name(p_table_owner,p_target_table), sql_builder_pkg.get_sql(sql_query_c1)));
-        
+
     end insert_to_target;
-        
+
 BEGIN
-    error_pkg.assert(check_schemas(p_cdc_table, p_stage_table) and check_schemas(p_cdc_table, p_target_table), 'SCHEMAS BETWEEN PROCESSING TABLES ARE NOT THE SAME. PLEASE INVESTIGATE');
-    
+    assert_pkg.is_true(check_schemas(p_cdc_table, p_stage_table) and check_schemas(p_cdc_table, p_target_table), 'SCHEMAS BETWEEN PROCESSING TABLES ARE NOT THE SAME. PLEASE INVESTIGATE');
+
     --debug_pkg.debug_on;
-    
+
     step_separate('CDC_COLUMNS');
     gather_cdc_columns(cdc_list);
     print_collection(cdc_list);
-    
+
     print_extra_line;
 
     step_separate('NON_CDC_COLUMNS');
     gather_non_cdc_columns(non_cdc_list);
     print_collection(non_cdc_list);
-    
+
     print_extra_line;
 
     step_separate('STG_TO_CDC --> Lets assume that data is loaded into CDC first (before stg)');
@@ -420,8 +421,8 @@ BEGIN
     print_extra_line;
 
     step_separate('INSERT FROM STAGE TO TARGET');
-    INSERT_TO_TARGET;
-    
+    insert_to_target;
+
     --debug_pkg.debug_off;
 
 exception
