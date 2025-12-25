de91286795aaebcf4252075a6ebd6655c0949aff
Trevor Little <trevor.j.little@gmail.com>
2025-12-24 23:31:46 -0500

    refactored query to use less steps

--- Oracle/SQL/map_user_source_package_body_procedures.sql
+++ Oracle/SQL/map_user_source_package_body_procedures.sql
@@ -1,41 +1,26 @@
-with ds as(
-    select 
-    substr(upper(trim(text)), 1, instr(upper(trim(text)), ' ')-1) as pl_type
-    , trim(
-        substr(
-            upper(trim(text)),
-            instr(upper(trim(text)), ' '),
-            decode(
-                instr(upper(trim(text)), ';'), 0, length(upper(trim(text))), instr(upper(trim(text)), ';')
-            ) - instr(upper(trim(text)), ' ')
-        )
-    )
-    as module
-    , a.* from user_source a
-    where upper(name) = upper('DATE_UTILS_PKG') and
+with ds as (
+    select case when regexp_like(upper(trim(text)), '(^PROCEDURE|^FUNCTION|^PACKAGE)') then 1 else 2 end as rn
+    , upper(trim(text)) as txt
+    --row_number() over (partition by case when pl_type in('FUNCTION', 'PROCEDURE') then d.module else null end order by line) as rn,
+    , d.*
+    from user_source d
+    where upper(name) = upper('salary_data_tapi') and
     type = 'PACKAGE BODY'
+    order by line
 )
 
-, format_output as (
-    select
-    case when rn = 1 then module else null end as mo,
-      n.*
-    from(
-        select decode(pl_type, 'FUNCTION', 1, 'PROCEDURE', 1, 2) as rn,
-        --row_number() over (partition by case when pl_type in('FUNCTION', 'PROCEDURE') then d.module else null end order by line) as rn,
-        d.*
-        from ds d
-        order by line
-    )n
+, formatted as (
+    select rn
+    , case when rn = 1 then substr(txt, 1, decode(instr(txt, '('), 0, length(txt),instr(txt, '(')-1)) else null end as header
+    , text
+    , line
+    from ds
 )
 
-, code_mp as (
-    select last_value(mo ignore nulls) over (order by line asc rows between unbounded preceding and current row) as cde,
-    a.* from format_output a
-    order by line asc
-)
+select * from (
+    select last_value(header ignore nulls) over (order by line asc rows between unbounded preceding and current row) as mapping
+    , a.*
+    from formatted a
+)--where mapping is not null
+;
 
-select substr(cde, 1, decode(instr(cde, '('), 0, length(cde),instr(cde, '(')-1)) as module
-, line
-, text
-from code_mp a;
